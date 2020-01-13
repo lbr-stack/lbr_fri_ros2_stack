e45d152d09687ef8a75826268f3562880e320643
mhubii <m.huber_1994@hotmail.de>
2020-01-13 19:36:22 +0000

    added gazebo launch, ros2 communication to do

---
+++ lbr_gazebo/launch/iiwa7_gazebo.launch.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir
+from launch.actions import ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
+    world_file_name = 'iiwa7.world'
+    world = os.path.join(get_package_share_directory('lbr_gazebo'), 'worlds', world_file_name)
+    launch_file_dir = os.path.join(get_package_share_directory('lbr_gazebo'), 'launch')
+
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_init.so'],
+            output='screen'),
+
+        ExecuteProcess(
+            cmd=['ros2', 'param', 'set', '/gazebo', 'use_sim_time', use_sim_time],
+            output='screen'),
+    ])
+
