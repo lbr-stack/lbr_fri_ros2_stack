393af770aeb0f46beec863e0aa868fc6c81264ce
mhubii <esteve@apache.org>
2020-01-17 20:48:22 +0000

    add med res stl

--- lbr_gazebo/launch/iiwa14_gazebo.launch.py
+++ lbr_gazebo/launch/iiwa14_gazebo.launch.py
@@ -3,14 +3,18 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import ThisLaunchFileDir
 from launch.actions import ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 
-
 def generate_launch_description():
+    models_dir = os.path.join(get_package_share_directory('lbr_gazebo'), 'models')
+
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] = os.environ['GAZEBO_MODEL_PATH'] + ':' + models_dir
+    else:
+        os.environ['GAZEBO_MODEL_PATH'] = models_dir
+
     use_sim_time = LaunchConfiguration('use_sim_time', default='True')
     world_file_name = 'iiwa14.world'
     world = os.path.join(get_package_share_directory('lbr_gazebo'), 'worlds', world_file_name)
