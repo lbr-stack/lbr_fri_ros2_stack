1b4f9b4398588268ffb75aec6c4ec87bdf72ec0c
mhubii <m.huber_1994@hotmail.de>
2020-01-23 15:34:47 +0000

    added lbr intermediate and msgs

--- lbr_fri_ros/src/lbr.h
+++ lbr_fri_ros/src/lbr.h
@@ -1,93 +1,56 @@
 #pragma once
 
 #include <mutex>
+#include <eigen3/Eigen/Core>
 
-#include <fri/friLBRState.h>
-#include <fri/friLBRCommand.h>
+using Joints = Eigen::Matrix<double, uint(KUKA::FRI::LBRState::NUMBER_OF_JOINTS), 1>;
 
+struct State {
+    State() = default;
+    State(Joints& ja, Joints& jt, uint ts) : joint_angles(ja), joint_torques(jt), time_stamp(ts) { };
+    State(const double* ja, const double* jt, uint ts) : joint_angles(ja), joint_torques(jt), time_stamp(ts) {  };
+    
+    Joints joint_angles = Joints::Zero();
+    Joints joint_torques = Joints::Zero();
+    uint time_stamp = 0;
+
+};
+
+// TODO: add tests for 2 threads that access and manipulate the data
+//       test size of joints angles and torque
+//       test get and set without threads
 class LBR {
     public:
-        LBR() = default;
+        LBR() : n_joints_(uint(KUKA::FRI::LBRState::NUMBER_OF_JOINTS)) {    };
 
-        auto inline get_state() -> const KUKA::FRI::LBRState& {
-            std::lock_guard<std::mutex> lk(mutex_);
-            return lbr_state_;
+        inline auto get_current_state() const -> const State& {
+            return current_state_;
         };
 
-        auto set_state(const KUKA::FRI::LBRState lbr_state) -> void {
-            std::lock_guard<std::mutex> lk(mutex_);
-            lbr_state_ = lbr_state;
+        inline auto get_commanded_state() const -> const State& {
+            return commanded_state_;
+        };
+
+        auto set_current_state(State& s) -> void {
+            std::lock_guard<std::mutex> lk(current_mutex_);
+
+            current_state_ = s;
+        };
+
+        auto set_commanded_state(State& s) {
+            std::lock_guard<std::mutex> lk(commanded_mutex_);
+
+            commanded_state_ = s;
         };
 
     private:
-        std::mutex mutex_;
+        uint n_joints_;
 
-        KUKA::FRI::LBRState lbr_state_;
+        // current
+        std::mutex current_mutex_;
+        State current_state_;
+
+        // commanded
+        std::mutex commanded_mutex_;
+        State commanded_state_;
 };
-
-// using state = std::tuple<double* /*joint angles*/, double* /*torque*/, uint /*time stamp*/>;
-
-// // TODO: add tests for 2 threads that access and manipulate the data
-// //       test size of joints angles and torque
-// //       test get and set without threads
-// class LBR {
-//     public:
-//         LBR() {
-//             current_joint_angles_ = new double[KUKA::FRI::LBRState::NUMBER_OF_JOINTS];
-//             current_torques_ = new double[KUKA::FRI::LBRState::NUMBER_OF_JOINTS];
-
-//             commanded_joint_angles_ = new double[KUKA::FRI::LBRState::NUMBER_OF_JOINTS];
-//             commanded_torques_ = new double[KUKA::FRI::LBRState::NUMBER_OF_JOINTS];
-//         };
-
-//         ~LBR() {
-//             delete[] current_joint_angles_;
-//             delete[] current_torques_;
-
-//             delete[] commanded_joint_angles_;
-//             delete[] commanded_torques_;
-//         }
-
-//         inline auto get_current_state() const -> const state {
-//             return std::make_tuple(current_joint_angles_, current_torques_, current_time_stamp_);
-//         };
-
-//         inline auto get_commanded_state() const -> const state {
-//             return std::make_tuple(commanded_joint_angles_, commanded_torques_, commanded_time_stamp_);
-//         };
-
-//         auto set_current_state(const double* joint_angles, const double* torques, const int time_stamp) -> void {
-//             std::lock_guard<std::mutex> lk(current_mutex_);
-            
-//             for (uint i = 0; i < KUKA::FRI::LBRState::NUMBER_OF_JOINTS; i++) {
-//                 current_joint_angles_[i] = joint_angles[i];
-//                 current_torques_[i] = torques[i];
-//             }
-
-//             current_time_stamp_ = time_stamp;
-//         };
-
-//         auto set_commanded_state(const double* joint_angles, const double* torques, const int time_stamp) {
-//             std::lock_guard<std::mutex> lk(commanded_mutex_);
-
-//             for (uint i = 0; i < KUKA::FRI::LBRState::NUMBER_OF_JOINTS; i++) {
-//                 commanded_joint_angles_[i] = joint_angles[i];
-//                 commanded_torques_[i] = torques[i];
-//             }
-
-//             commanded_time_stamp_ = time_stamp;
-//         };
-
-//     private:
-//         // current
-//         std::mutex current_mutex_;
-//         double* current_joint_angles_;
-//         double* current_torques_;
-//         uint current_time_stamp_;
-
-//         // commanded
-//         std::mutex commanded_mutex_;
-//         double* commanded_joint_angles_;
-//         double* commanded_torques_;
-//         uint commanded_time_stamp_;
-// };
