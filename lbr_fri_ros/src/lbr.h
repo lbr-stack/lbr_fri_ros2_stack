89d7a6c7cf4037052dd4c1ba3f01b97a8d16093c
mhubii <m.huber_1994@hotmail.de>
2020-01-23 17:25:43 +0000

    removed Eigen dependency

--- lbr_fri_ros/src/lbr.h
+++ lbr_fri_ros/src/lbr.h
@@ -1,56 +1,42 @@
 #pragma once
 
 #include <mutex>
-#include <eigen3/Eigen/Core>
-
-using Joints = Eigen::Matrix<double, uint(KUKA::FRI::LBRState::NUMBER_OF_JOINTS), 1>;
-
-struct State {
-    State() = default;
-    State(Joints& ja, Joints& jt, uint ts) : joint_angles(ja), joint_torques(jt), time_stamp(ts) { };
-    State(const double* ja, const double* jt, uint ts) : joint_angles(ja), joint_torques(jt), time_stamp(ts) {  };
-    
-    Joints joint_angles = Joints::Zero();
-    Joints joint_torques = Joints::Zero();
-    uint time_stamp = 0;
-
-};
+#include <lbr_msgs/msg/lbr_state.hpp>
 
 // TODO: add tests for 2 threads that access and manipulate the data
 //       test size of joints angles and torque
 //       test get and set without threads
+
 class LBR {
     public:
-        LBR() : n_joints_(uint(KUKA::FRI::LBRState::NUMBER_OF_JOINTS)) {    };
+        LBR() = default;
 
-        inline auto get_current_state() const -> const State& {
+        inline auto get_current_state() const -> const lbr_msgs::msg::LBRState& {
             return current_state_;
         };
 
-        inline auto get_commanded_state() const -> const State& {
+        inline auto get_commanded_state() const -> const lbr_msgs::msg::LBRState& {
             return commanded_state_;
         };
 
-        auto set_current_state(State& s) -> void {
+        auto set_current_state(lbr_msgs::msg::LBRState& s) -> void {
             std::lock_guard<std::mutex> lk(current_mutex_);
 
             current_state_ = s;
         };
 
-        auto set_commanded_state(State& s) {
+        auto set_commanded_state(lbr_msgs::msg::LBRState& s) {
             std::lock_guard<std::mutex> lk(commanded_mutex_);
 
             commanded_state_ = s;
         };
 
     private:
-        uint n_joints_;
-
         // current
         std::mutex current_mutex_;
-        State current_state_;
+        lbr_msgs::msg::LBRState current_state_;
 
         // commanded
         std::mutex commanded_mutex_;
-        State commanded_state_;
+        lbr_msgs::msg::LBRState commanded_state_;
 };
--- lbr_fri_ros/src/lbr_client.h
+++ lbr_fri_ros/src/lbr_client.h
@@ -4,25 +4,31 @@
 
 #include <fri/friLBRClient.h>
 #include <lbr.h>
+#include <lbr_msgs/msg/lbr_state.hpp>
 
 class LBRClient : public KUKA::FRI::LBRClient {
     public:
-        LBRClient(std::shared_ptr<LBR> lbr)
-            : lbr_(lbr) {
-            
-        };
+        LBRClient(std::shared_ptr<LBR> lbr) : lbr_(lbr) {   };
 
         void monitor() override {
-            lbr_->set_state(robotState());
+            const double* ja = robotState().getMeasuredJointPosition();
+            state_.joint_positions.assign(ja, ja+KUKA::FRI::LBRState::NUMBER_OF_JOINTS);
+            const double* t = robotState().getMeasuredTorque();
+            state_.torques.assign(t, t+KUKA::FRI::LBRState::NUMBER_OF_JOINTS);
+            state_.time_stamp = robotState().getTimestampNanoSec();
+
+            lbr_->set_current_state(state_);
         };
 
         void command() override {
             switch (robotState().getClientCommandMode()) {
                 case KUKA::FRI::POSITION:
-                    robotCommand().setJointPosition(lbr_->get_state().getCommandedJointPosition());
+                    if (!lbr_->get_commanded_state().joint_positions.data())
+                        robotCommand().setJointPosition(lbr_->get_commanded_state().joint_positions.data());
                     break;
                 case KUKA::FRI::TORQUE:
-                    robotCommand().setTorque(lbr_->get_state().getCommandedTorque());
+                    if (!lbr_->get_commanded_state().torques.data())
+                        robotCommand().setTorque(lbr_->get_commanded_state().torques.data());
                     break;
                 case KUKA::FRI::WRENCH:
                     printf("Wrench command mode not supported./n");
@@ -33,4 +39,5 @@ class LBRClient : public KUKA::FRI::LBRClient {
 
     private:
         std::shared_ptr<LBR> lbr_;
+        lbr_msgs::msg::LBRState state_;
 };
