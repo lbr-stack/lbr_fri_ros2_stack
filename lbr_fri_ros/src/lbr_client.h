f53fc754f7023c71e10ecfa4d501db9a02e6e44f
mhubii <m.huber_1994@hotmail.de>
2020-01-24 12:26:58 +0000

    added state feedback and update for every state

--- lbr_fri_ros/src/lbr_client.h
+++ lbr_fri_ros/src/lbr_client.h
@@ -2,6 +2,7 @@
 
 #include <memory>
 
+#include <rclcpp/rclcpp.hpp>
 #include <fri/friLBRClient.h>
 #include <lbr.h>
 #include <lbr_msgs/msg/lbr_state.hpp>
@@ -10,7 +11,43 @@ class LBRClient : public KUKA::FRI::LBRClient {
     public:
         LBRClient(std::shared_ptr<LBR> lbr) : lbr_(lbr) {   };
 
-        void monitor() override {
+        auto onStateChange(KUKA::FRI::ESessionState old_mode, KUKA::FRI::ESessionState new_mode) -> void override {
+            printf("LBR switched from %s to %s.\n", translate(old_mode).c_str(), translate(new_mode).c_str());
+        };
+
+        auto monitor() -> void override {
+            update_state();
+        };
+
+        auto waitForCommand() -> void override {
+            update_state();
+            KUKA::FRI::LBRClient::waitForCommand();
+        };
+
+        auto command() -> void override {
+            update_state();
+
+            switch (robotState().getClientCommandMode()) {
+                case KUKA::FRI::POSITION:
+                    if (!lbr_->get_commanded_state().joint_positions.empty())
+                        robotCommand().setJointPosition(lbr_->get_commanded_state().joint_positions.data());
+                    else
+                        KUKA::FRI::LBRClient::command();
+                    break;
+                case KUKA::FRI::TORQUE:
+                    if (!lbr_->get_commanded_state().torques.empty())
+                        robotCommand().setTorque(lbr_->get_commanded_state().torques.data());
+                    else
+                        KUKA::FRI::LBRClient::command();
+                    break;
+                case KUKA::FRI::WRENCH:
+                    printf("Wrench command mode not supported.\n");
+                default:
+                    break;
+            }
+        };
+
+        auto update_state() -> void {
             const double* ja = robotState().getMeasuredJointPosition();
             state_.joint_positions.assign(ja, ja+KUKA::FRI::LBRState::NUMBER_OF_JOINTS);
             const double* t = robotState().getMeasuredTorque();
@@ -18,24 +55,24 @@ class LBRClient : public KUKA::FRI::LBRClient {
             state_.time_stamp = robotState().getTimestampNanoSec();
 
             lbr_->set_current_state(state_);
-        };
+        }
 
-        void command() override {
-            switch (robotState().getClientCommandMode()) {
-                case KUKA::FRI::POSITION:
-                    if (!lbr_->get_commanded_state().joint_positions.data())
-                        robotCommand().setJointPosition(lbr_->get_commanded_state().joint_positions.data());
-                    break;
-                case KUKA::FRI::TORQUE:
-                    if (!lbr_->get_commanded_state().torques.data())
-                        robotCommand().setTorque(lbr_->get_commanded_state().torques.data());
-                    break;
-                case KUKA::FRI::WRENCH:
-                    printf("Wrench command mode not supported./n");
+        auto translate(int mode) -> std::string {
+            switch (mode) {
+                case KUKA::FRI::ESessionState::IDLE:
+                    return "idle";
+                case KUKA::FRI::ESessionState::MONITORING_WAIT:
+                    return "monitoring wait";
+                case KUKA::FRI::ESessionState::MONITORING_READY:
+                    return "monitoring ready";
+                case KUKA::FRI::ESessionState::COMMANDING_WAIT:
+                    return "commanding wait";
+                case KUKA::FRI::ESessionState::COMMANDING_ACTIVE:
+                    return "commanding active";
                 default:
-                    break;
+                    return "undefined";
             }
-        };
+        }
 
     private:
         std::shared_ptr<LBR> lbr_;
