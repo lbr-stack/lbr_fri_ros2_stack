d2e394834356304391e2f630fd9f9c36a3977120
mhubii <m.huber_1994@hotmail.de>
2022-02-07 18:27:26 +0000

    added a no sleep control node

---
+++ lbr_hardware/src/fri_ros2_control_node.cpp
@@ -0,0 +1,53 @@
+// Copyright 2020 ROS2-Control Development Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
+#include <string>
+#include <thread>
+
+#include "controller_manager/controller_manager.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+using namespace std::chrono_literals;
+
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+
+  std::shared_ptr<rclcpp::Executor> executor =
+    std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
+  std::string manager_node_name = "controller_manager";
+
+  auto cm = std::make_shared<controller_manager::ControllerManager>(executor, manager_node_name);
+
+  // TODO(anyone): Due to issues with the MutliThreadedExecutor, this control loop does not rely on
+  // the executor (see issue #260).
+  // When the MutliThreadedExecutor issues are fixed (ros2/rclcpp#1168), this loop should be
+  // converted back to a timer.
+  std::thread cm_thread([cm]() {
+    while (rclcpp::ok())
+    {
+      cm->read();
+      cm->update();
+      cm->write();
+    }
+  });
+
+  executor->add_node(cm);
+  executor->spin();
+  cm_thread.join();
+  rclcpp::shutdown();
+  return 0;
+}
