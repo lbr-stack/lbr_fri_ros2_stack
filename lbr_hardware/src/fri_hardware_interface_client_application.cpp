9b8512bc9412ad659a08d48f329fce900f00906e
mhubii <m.huber_1994@hotmail.de>
2022-02-12 16:56:19 +0000

    removed typo

--- lbr_hardware/src/fri_hardware_interface_client_application.cpp
+++ lbr_hardware/src/fri_hardware_interface_client_application.cpp
@@ -11,7 +11,7 @@ namespace KUKA {
 
         bool FRIHardwareInterfaceClientApplication::receiveAndDecode() {
             if (!_connection.isOpen()) {
-                RCLCPP_ERROR(rclcpp::get_logger(CLIENT_APPLIATION_LOGGER), "Client application is not connected.");
+                RCLCPP_ERROR(rclcpp::get_logger(CLIENT_APPLICATION_LOGGER), "Client application is not connected.");
                 return false;
             }
 
@@ -21,11 +21,12 @@ namespace KUKA {
             buffer_size_ = _connection.receive(_data->receiveBuffer, FRI_MONITOR_MSG_MAX_SIZE);
 
             if (buffer_size_ <= 0) {  // TODO: buffer_size_ == 0 -> connection closed (maybe go to IDLE instead of stopping?)
-                RCLCPP_ERROR(rclcpp::get_logger(CLIENT_APPLIATION_LOGGER), "Failed while trying to receive monitoring message");
+                RCLCPP_ERROR(rclcpp::get_logger(CLIENT_APPLICATION_LOGGER), "Failed while trying to receive monitoring message");
                 return false;
             }
 
             if (!_data->decoder.decode(_data->receiveBuffer, buffer_size_)) {
+                RCLCPP_ERROR(rclcpp::get_logger(CLIENT_APPLICATION_LOGGER), "Failed while trying to decode data.");
                 return false;
             }
 
@@ -33,7 +34,7 @@ namespace KUKA {
             if (_data->expectedMonitorMsgID != _data->monitoringMsg.header.messageIdentifier)
             {
                 RCLCPP_ERROR(
-                    rclcpp::get_logger(CLIENT_APPLIATION_LOGGER),
+                    rclcpp::get_logger(CLIENT_APPLICATION_LOGGER),
                     "Error: incompatible IDs for received message (got: %d expected %d).",
                     (int)_data->monitoringMsg.header.messageIdentifier,
                     (int)_data->expectedMonitorMsgID
@@ -93,15 +94,15 @@ namespace KUKA {
 
                 // set sequence counters
                 _data->commandMsg.header.sequenceCounter = _data->sequenceCounter++;
-                _data->commandMsg.header.reflectedSequenceCounter = 
-                _data->monitoringMsg.header.sequenceCounter;
+                _data->commandMsg.header.reflectedSequenceCounter = _data->monitoringMsg.header.sequenceCounter;
 
                 if (!_data->encoder.encode(_data->sendBuffer, buffer_size_)) {
+                    RCLCPP_ERROR(rclcpp::get_logger(CLIENT_APPLICATION_LOGGER),"Failed while trying to encode data.");
                     return false;
                 }
 
                 if (!_connection.send(_data->sendBuffer, buffer_size_)) {
-                    printf("Error: failed while trying to send command message!\n");
+                    RCLCPP_ERROR(rclcpp::get_logger(CLIENT_APPLICATION_LOGGER),"Failed while trying to send command message.");
                     return false;
                 }
             }
