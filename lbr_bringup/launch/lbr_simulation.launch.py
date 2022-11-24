5d3df908a05c40dc9428983b7fcbaa4a2ac7adcd
mhubii <m.huber_1994@hotmail.de>
2022-11-24 12:53:40 +0100

    removed rt-priority from default

--- lbr_bringup/launch/lbr_simulation.launch.py
+++ lbr_bringup/launch/lbr_simulation.launch.py
@@ -1,9 +1,9 @@
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
-from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
-from launch_ros.substitutions import FindPackageShare
-from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
