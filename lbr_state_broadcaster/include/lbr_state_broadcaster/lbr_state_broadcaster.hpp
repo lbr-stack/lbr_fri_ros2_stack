175620c765b0725b91ff7e2f93a0633cc7c3f838
mhubii <m.huber_1994@hotmail.de>
2022-11-25 19:59:21 +0100

    finished lbr state broadcaster

--- lbr_state_broadcaster/include/lbr_state_broadcaster/lbr_state_broadcaster.hpp
+++ lbr_state_broadcaster/include/lbr_state_broadcaster/lbr_state_broadcaster.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
 #include <memory>
-#include <string>
 #include <vector>
+#include <stdexcept>
+#include <string>
 
 #include <controller_interface/controller_interface.hpp>
+#include <hardware_interface/types/hardware_interface_type_values.hpp>
 #include <pluginlib/class_list_macros.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <realtime_tools/realtime_publisher.h>
 
+#include <lbr_hardware/fri_hardware_interface_type_values.hpp>
 #include <lbr_state_msgs/msg/lbr_state.hpp>
 
 
@@ -40,9 +43,22 @@ class LBRStateBroadcaster : public controller_interface::ControllerInterface {
         ) override;
 
     protected:
+        bool use_local_topics_;
+        std::string lbr_state_topic_;
+
         std::vector<std::string> joint_names_;
         std::shared_ptr<rclcpp::Publisher<lbr_state_msgs::msg::LBRState>> lbr_state_publisher_;
         std::shared_ptr<realtime_tools::RealtimePublisher<lbr_state_msgs::msg::LBRState>> realtime_lbr_state_publisher_;
+        lbr_state_msgs::msg::LBRState lbr_state_;
+
+        // references to FRI exposed states for convenience
+        std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>> position_interfaces_;
+        std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>> effort_interfaces_;
+        std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>> external_torque_interfaces_;
+
+        std::unique_ptr<hardware_interface::LoanedStateInterface> sample_time_interface_ptr_;
+        std::unique_ptr<hardware_interface::LoanedStateInterface> time_stamp_sec_interface_ptr_;
+        std::unique_ptr<hardware_interface::LoanedStateInterface> time_stamp_nano_sec_interface_ptr_;
 };
 
 } // end of namespace lbr_state_broadcaster
