8c486fb39f30230729d83e52bb5b77f0db9fd669
mhubii <m.huber_1994@hotmail.de>
2022-11-30 13:09:33 +0000

    updated examples

--- lbr_examples/scripts/lbr_sinusoidal_node.py
+++ lbr_examples/scripts/lbr_sinusoidal_node.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from typing import Union
 import math
 from copy import deepcopy
 
@@ -12,61 +13,60 @@ from lbr_state_msgs.msg import LBRState
 
 class LBRSinusoidalNode(Node):
     _sim: bool
-    _command_rate: int
     _amplitude: float
     _period: float
     _t0: float
-    _initial_joint_states: JointState
+    _initial_state: Union[JointState, LBRState]
 
     def __init__(self, node_name: str="lbr_read_write_node"):
         super().__init__(node_name)
 
         # declare and get parameters
         self.declare_parameter("sim", False)
-        self.declare_parameter("command_rate", 100)
         self.declare_parameter("amplitude", math.pi/4.)
         self.declare_parameter("period", 10.)
 
-        self._sim = self.get_parameter("sim")
-        self._command_rate = int(self.get_parameter("command_rate").value)
+        self._sim = bool(self.get_parameter("sim").value)
         self._amplitude = float(self.get_parameter("amplitude").value)
         self._period = float(self.get_parameter("period").value)
         self._t0 = None
-        self._initial_joint_states = None
+        self._initial_state = None
 
         # create publishers and subscribers
-        self._joint_states_subscriber = self.create_subscription(
-            JointState, "/joint_states", self._joint_states_callback, 1
-        )
-
-        if not self._sim:
+        if self._sim:
+            # careful when using joint states and sending commands, as they
+            # are not sorted! https://github.com/ros-controls/ros2_controllers/issues/159
+            # using the /joint_states might cause unintended behavior on the robot
+            # use the /lbr_state instead
+            self._joint_states_subscriber = self.create_subscription(
+                JointState, "/joint_states", self._joint_states_callback, 1
+            )
+        else:
             self._lbr_state_subscriber = self.create_subscription(
                 LBRState, "/lbr_state", self._lbr_state_callback, 1
             )
-
-        self._position_command_timer = self.create_timer(
-            1./self._command_rate, self._position_command_timer_callback
-        )
 
         self._position_command_publisher = self.create_publisher(
             Float64MultiArray, "/forward_position_controller/commands", 1
         )
 
     def _joint_states_callback(self, msg: JointState) -> None:
-        self.get_logger().info("Got position:\n{}".format(msg.position))
         # get the initial joint configuration and time
-        if not self._initial_joint_states:
+        if not self._initial_state:
             self._t0 = float(self.get_clock().now().nanoseconds)
-            self._initial_joint_states = msg
+            self._initial_state = msg
+        self._execute_command()
 
     def _lbr_state_callback(self, msg: LBRState) -> None:
-        self.get_logger().info("Got external torques:\n{}".format(msg.external_torque))
+        # get the initial joint configuration and time
+        if not self._initial_state:
+            self._t0 = float(self.get_clock().now().nanoseconds)
+            self._initial_state = msg
+        self._execute_command()
 
-    def _position_command_timer_callback(self):
+    def _execute_command(self) -> None:
         command = Float64MultiArray()
-        if not self._initial_joint_states:
-            return
-        command.data = deepcopy(self._initial_joint_states.position)
+        command.data = deepcopy(self._initial_state.position)
 
         omega = 2*math.pi/self._period
         t = (float(self.get_clock().now().nanoseconds) - self._t0)/1.e9
