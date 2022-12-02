fd0feb69046c10ace4d1ad6166d93c0f59be9eed
mhubii <m.huber_1994@hotmail.de>
2022-12-02 17:37:46 +0000

    added full state representation

--- lbr_fri_ros2/src/lbr_state_client.cpp
+++ lbr_fri_ros2/src/lbr_state_client.cpp
@@ -84,12 +84,12 @@ namespace lbr_hardware
 
     void LBRStateClient::lbr_command_cb_(const lbr_fri_msgs::msg::LBRCommand::SharedPtr lbr_command)
     {
-        if (lbr_command->e_client_command_mode != robotState().getClientCommandMode())
+        if (lbr_command->client_command_mode != robotState().getClientCommandMode())
         {
             RCLCPP_WARN(
                 node_->get_logger(),
                 "Expected client command mode %d, got %d.", robotState().getClientCommandMode(),
-                lbr_command->e_client_command_mode);
+                lbr_command->client_command_mode);
             reset_lbr_command_(lbr_command_);
             return;
         }
@@ -107,29 +107,49 @@ namespace lbr_hardware
 
     void LBRStateClient::robot_state_to_lbr_state_()
     {
+        lbr_state_.sample_time = robotState().getSampleTime();
+        lbr_state_.session_state = robotState().getSessionState();
+        lbr_state_.connection_quality = robotState().getConnectionQuality();
+        lbr_state_.safety_state = robotState().getSafetyState();
+        lbr_state_.operation_mode = robotState().getOperationMode();
+        lbr_state_.drive_state = robotState().getDriveState();
+        lbr_state_.client_command_mode = robotState().getClientCommandMode();
+        lbr_state_.overlay_type = robotState().getOverlayType();
+        lbr_state_.control_mode = robotState().getControlMode();
+
+        lbr_state_.time_stamp_sec = robotState().getTimestampSec();
+        lbr_state_.time_stamp_nano_sec = robotState().getTimestampNanoSec();
+
         auto *measured_joint_position = robotState().getMeasuredJointPosition();
         lbr_state_.measured_joint_position.assign(measured_joint_position, measured_joint_position + KUKA::FRI::LBRState::NUMBER_OF_JOINTS);
+
+        auto *commanded_joint_position = robotState().getCommandedJointPosition();
+        lbr_state_.commanded_joint_position.assign(commanded_joint_position, commanded_joint_position + KUKA::FRI::LBRState::NUMBER_OF_JOINTS);
+
+        auto *measured_torque = robotState().getMeasuredTorque();
+        lbr_state_.measured_torque.assign(measured_torque, measured_torque + KUKA::FRI::LBRState::NUMBER_OF_JOINTS);
+
+        auto *commanded_torque = robotState().getCommandedTorque();
+        lbr_state_.commanded_torque.assign(commanded_torque, commanded_torque + KUKA::FRI::LBRState::NUMBER_OF_JOINTS);
+
+        auto *external_torque = robotState().getExternalTorque();
+        lbr_state_.external_torque.assign(external_torque, external_torque + KUKA::FRI::LBRState::NUMBER_OF_JOINTS);
+
         if (robotState().getSessionState() == KUKA::FRI::ESessionState::COMMANDING_WAIT ||
             robotState().getSessionState() == KUKA::FRI::ESessionState::COMMANDING_ACTIVE)
         {
             auto *ipo_joint_position = robotState().getIpoJointPosition();
             lbr_state_.ipo_joint_position.assign(ipo_joint_position, ipo_joint_position + KUKA::FRI::LBRState::NUMBER_OF_JOINTS);
         }
-        auto *torque = robotState().getMeasuredTorque();
-        lbr_state_.torque.assign(torque, torque + KUKA::FRI::LBRState::NUMBER_OF_JOINTS);
-        auto *external_torque = robotState().getExternalTorque();
-        lbr_state_.external_torque.assign(external_torque, external_torque + KUKA::FRI::LBRState::NUMBER_OF_JOINTS);
-        lbr_state_.sample_time = robotState().getSampleTime();
-        lbr_state_.time_stamp_sec = robotState().getTimestampSec();
-        lbr_state_.time_stamp_nano_sec = robotState().getTimestampNanoSec();
-        lbr_state_.e_client_command_mode = robotState().getClientCommandMode();
+
+        lbr_state_.tracking_performance = robotState().getTrackingPerformance();
     }
 
     void LBRStateClient::init_lbr_command_(lbr_fri_msgs::msg::LBRCommand &lbr_command)
     {
         try
         {
-            lbr_command.e_client_command_mode = std::numeric_limits<int>::quiet_NaN();
+            lbr_command.client_command_mode = std::numeric_limits<int>::quiet_NaN();
             lbr_command.joint_position.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
             lbr_command.torque.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
             lbr_command.wrench.force.x = std::numeric_limits<double>::quiet_NaN();
@@ -150,13 +170,26 @@ namespace lbr_hardware
     {
         try
         {
-            lbr_state.measured_joint_position.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
-            lbr_state.ipo_joint_position.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
-            lbr_state.torque.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
-            lbr_state.external_torque.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
             lbr_state.sample_time = std::numeric_limits<double>::quiet_NaN();
-            lbr_state.time_stamp_sec = std::numeric_limits<uint32_t>::quiet_NaN();
-            lbr_state.time_stamp_nano_sec = std::numeric_limits<uint32_t>::quiet_NaN();
+            lbr_state.session_state = std::numeric_limits<int8_t>::quiet_NaN();
+            lbr_state.connection_quality = std::numeric_limits<int8_t>::quiet_NaN();
+            lbr_state.safety_state = std::numeric_limits<int8_t>::quiet_NaN();
+            lbr_state.operation_mode = std::numeric_limits<int8_t>::quiet_NaN();
+            lbr_state.drive_state = std::numeric_limits<int8_t>::quiet_NaN();
+            lbr_state.client_command_mode = std::numeric_limits<int8_t>::quiet_NaN();
+            lbr_state.overlay_type = std::numeric_limits<int8_t>::quiet_NaN();
+            lbr_state.control_mode = std::numeric_limits<int8_t>::quiet_NaN();
+
+            lbr_state.time_stamp_sec = std::numeric_limits<int32_t>::quiet_NaN();
+            lbr_state.time_stamp_nano_sec = std::numeric_limits<int32_t>::quiet_NaN();
+
+            lbr_state.measured_joint_position.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
+            lbr_state.commanded_joint_position.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
+            lbr_state.measured_torque.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
+            lbr_state.commanded_torque.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
+            lbr_state.external_torque.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
+            lbr_state_.ipo_joint_position.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
+            lbr_state_.tracking_performance = std::numeric_limits<double>::quiet_NaN();
         }
         catch (const std::exception &e)
         {
@@ -174,7 +207,7 @@ namespace lbr_hardware
             {
                 throw std::length_error("Found lbr_command of invalid size.");
             }
-            lbr_command.e_client_command_mode = std::numeric_limits<int>::quiet_NaN();
+            lbr_command.client_command_mode = std::numeric_limits<int>::quiet_NaN();
             std::fill(lbr_command.joint_position.begin(), lbr_command.joint_position.end(), std::numeric_limits<double>::quiet_NaN());
             std::fill(lbr_command.torque.begin(), lbr_command.torque.end(), std::numeric_limits<double>::quiet_NaN());
             lbr_command.wrench.force.x = std::numeric_limits<double>::quiet_NaN();
@@ -195,16 +228,26 @@ namespace lbr_hardware
     {
         try
         {
+
+            lbr_state.measured_joint_position.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
+            lbr_state.commanded_joint_position.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
+            lbr_state.measured_torque.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
+            lbr_state.commanded_torque.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
+            lbr_state.external_torque.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
+            lbr_state_.ipo_joint_position.resize(KUKA::FRI::LBRState::NUMBER_OF_JOINTS, std::numeric_limits<double>::quiet_NaN());
+
             if (lbr_state.measured_joint_position.size() != KUKA::FRI::LBRState::NUMBER_OF_JOINTS ||
-                lbr_state.ipo_joint_position.size() != KUKA::FRI::LBRState::NUMBER_OF_JOINTS ||
-                lbr_state.torque.size() != KUKA::FRI::LBRState::NUMBER_OF_JOINTS ||
-                lbr_state.external_torque.size() != KUKA::FRI::LBRState::NUMBER_OF_JOINTS)
+                lbr_state.commanded_joint_position.size() != KUKA::FRI::LBRState::NUMBER_OF_JOINTS ||
+                lbr_state.measured_torque.size() != KUKA::FRI::LBRState::NUMBER_OF_JOINTS ||
+                lbr_state.commanded_torque.size() != KUKA::FRI::LBRState::NUMBER_OF_JOINTS ||
+                lbr_state.external_torque.size() != KUKA::FRI::LBRState::NUMBER_OF_JOINTS ||
+                lbr_state.ipo_joint_position.size() != KUKA::FRI::LBRState::NUMBER_OF_JOINTS)
             {
                 throw std::length_error("Found lbr_state of invalid size.");
             }
             std::fill(lbr_state.measured_joint_position.begin(), lbr_state.measured_joint_position.end(), std::numeric_limits<double>::quiet_NaN());
             std::fill(lbr_state.ipo_joint_position.begin(), lbr_state.ipo_joint_position.end(), std::numeric_limits<double>::quiet_NaN());
-            std::fill(lbr_state.torque.begin(), lbr_state.torque.end(), std::numeric_limits<double>::quiet_NaN());
+            std::fill(lbr_state.measured_torque.begin(), lbr_state.measured_torque.end(), std::numeric_limits<double>::quiet_NaN());
             std::fill(lbr_state.external_torque.begin(), lbr_state.external_torque.end(), std::numeric_limits<double>::quiet_NaN());
             lbr_state.sample_time = std::numeric_limits<double>::quiet_NaN();
             lbr_state.time_stamp_sec = std::numeric_limits<uint32_t>::quiet_NaN();
@@ -220,7 +263,7 @@ namespace lbr_hardware
     bool LBRStateClient::verify_lbr_command_(const lbr_fri_msgs::msg::LBRCommand &lbr_command)
     {
         // TODO: modify for different control modes
-        switch (lbr_command.e_client_command_mode)
+        switch (lbr_command.client_command_mode)
         {
         case KUKA::FRI::EClientCommandMode::POSITION:
             if (std::isnan(lbr_command.joint_position[0]))
