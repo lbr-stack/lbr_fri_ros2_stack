37503fb8dad0bfc77b04593fb4bbfdb8f83b4836
mhubii <m.huber_1994@hotmail.de>
2022-12-16 17:00:32 +0000

    added state smoothing node

--- lbr_fri_ros2/launch/lbr_spinner.launch.py
+++ lbr_fri_ros2/launch/lbr_spinner.launch.py
@@ -1,4 +1,6 @@
 from launch import LaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
 from launch_ros.actions import Node
 
 
@@ -10,4 +12,16 @@ def generate_launch_description():
         output="screen",
     )
 
-    return LaunchDescription([lbr_spinner_node])
+    lbr_state_smoothing_node = Node(
+        package="lbr_fri_ros2",
+        executable="lbr_state_smoothing_node.py",
+        output="screen",
+    )
+
+    lbr_state_smoothing_node_event_handler = RegisterEventHandler(
+        OnProcessStart(
+            target_action=lbr_spinner_node, on_start=[lbr_state_smoothing_node]
+        )
+    )
+
+    return LaunchDescription([lbr_spinner_node, lbr_state_smoothing_node_event_handler])
