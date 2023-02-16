6a0c8c590ed7e412ddcae133802861bdf76c5676
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-02-16 19:39:31 +0000

    Methods for retrieving current joint position/velocity/acceleration

--- lbr_python_tools/lbr_python_tools/lbr_node.py
+++ lbr_python_tools/lbr_python_tools/lbr_node.py
@@ -1,4 +1,5 @@
 import os
+import time
 import optas
 import rclpy
 from rclpy import qos
@@ -14,6 +15,10 @@ from lbr_fri_msgs.msg import LBRCommand, LBRState
 class StateListener:
     def __init__(self, node):
         self._node = node
+        self._t_prev = None
+        self._t = None
+        self._prev_prev_msg = None
+        self._prev_msg = None
         self._msg = None
         if self._node.sim:
             self._topic_cls = JointState
@@ -30,30 +35,75 @@ class StateListener:
         )
         if self._node.sim:
             self.get_position = self.get_joint_states_position
+            self.get_velocity = self.get_joint_states_velocity
+            self.get_acceleration = self.get_joint_states_acceleration
         else:
             self.get_position = self.get_lbr_states_position
+            self.get_velocity = self.get_lbr_states_velocity
+            self.get_acceleration = self.get_lbr_states_acceleration
 
     def _callback(self, msg):
+        self._prev_prev_msg = deepcopy(self._prev_msg)
+        self._prev_msg = deepcopy(self._msg)
         self._msg = msg
+        self._t_prev = deepcopy(self._t)
+        self._t = time.time()
 
     def _get(self):
         return deepcopy(self._msg)
 
+    def _get_prev(self):
+        return deepcopy(self._prev_msg)
+
+    def _get_prev_prev(self):
+        return deepcopy(self._prev_prev_msg)
+
     def recieved(self):
-        return self._msg is not None
+        return (
+            (self._msg is not None)
+            and (self._prev_msg is not None)
+            and (self._prev_prev_msg is not None)
+        )
 
     def get_lbr_states_position(self):
         return self._get().measured_joint_position
 
-    def get_joint_states_position(self):
-        msg = self._get()
+    def get_lbr_states_velocity(self):
+        curr = self._get().measured_joint_position
+        prev = self._get_prev().measured_joint_position
+        return [(c - p) / self._msg.sample_time for c, p in zip(curr, prev)]
 
+    def get_lbr_states_acceleration(self):
+        dt = self._msg.sample_time
+        curr = optas.np.array(self._get().measured_joint_position)
+        prev = optas.np.array(self._get_prev().measured_joint_position)
+        prev_prev = optas.np.array(self._get_prev_prev().measured_joint_position)
+        dcurr = (curr - prev) / dt
+        dprev = (prev - prev_prev) / dt
+        ddq = (dcurr - dprev) / dt
+        return ddq.tolist()
+
+    def _get_joint_states(self, msg, attr):
         # Ensure joint positions are in correct order
         q = []
+        x = getattr(msg, attr)
         for joint_name in self._node.model.actuated_joint_names:
             joint_idx = msg.name.index(joint_name)
-            q.append(msg.position[joint_idx])
+            q.append(x[joint_idx])
         return q
+
+    def get_joint_states_position(self):
+        return self._get_joint_states(self._get(), "position")
+
+    def get_joint_states_velocity(self):
+        return self._get_joint_states(self._get(), "velocity")
+
+    def get_joint_states_acceleration(self):
+        dt = self._t - self._t_prev
+        dqcurr = optas.np.array(self._get_joint_states(self._get(), "velocity"))
+        dqprev = optas.np.array(self._get_joint_states(self._get_prev(), "velocity"))
+        ddq = (dqcurr - dqprev) / dt
+        return ddq.tolist()
 
 
 class RobotCommander:
