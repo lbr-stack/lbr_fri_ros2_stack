8dcb1adb63ca742e1029e6ec6d4547762e0e942f
mhubii <m.huber_1994@hotmail.de>
2023-03-27 13:44:19 +0100

    removed nan checks as unused

--- lbr_fri_ros2/include/lbr_fri_ros2/lbr_intermediary.hpp
+++ lbr_fri_ros2/include/lbr_fri_ros2/lbr_intermediary.hpp
@@ -1,9 +1,6 @@
 #ifndef LBR_FRI_ROS2__LBR_INTERMEDIARY_HPP_
 #define LBR_FRI_ROS2__LBR_INTERMEDIARY_HPP_
 
-#include <array>
-#include <cmath>
-#include <limits>
 #include <stdexcept>
 
 #include "fri/FRIMessages.pb.h"
@@ -23,7 +20,7 @@ class LBRIntermediary {
   static constexpr auto WRENCH_ZEROS = WrenchArray{0.};
 
 public:
-  LBRIntermediary();
+  LBRIntermediary() = default;
 
   bool command_to_buffer(const lbr_fri_msgs::msg::LBRCommand::ConstSharedPtr lbr_command);
   bool buffer_to_command(KUKA::FRI::LBRCommand &lbr_command) const;
@@ -31,25 +28,9 @@ public:
   bool state_to_buffer(const KUKA::FRI::LBRState &lbr_state);
   bool buffer_to_state(lbr_fri_msgs::msg::LBRState &lbr_state) const;
 
-  void set_lbr_command_buffer_nan();
-  void set_lbr_state_buffer_nan();
-
 protected:
-  // reset buffers
-  bool reset_buffers_();
-  bool reset_lbr_command_buffer_();
-  bool reset_lbr_state_buffer_();
-
-  // validity checks
-  bool lbr_command_is_nan_(const lbr_fri_msgs::msg::LBRCommand::ConstSharedPtr lbr_command) const;
-  bool joint_position_command_is_nan_(const JointArray &joint_position_command) const;
-  bool wrench_command_is_nan_(const WrenchArray &wrench_command) const;
-  bool torque_command_is_nan_(const JointArray &torque_command) const;
-
-  bool lbr_state_is_nan_(const lbr_fri_msgs::msg::LBRState::ConstSharedPtr lbr_state) const;
-
-  lbr_fri_msgs::msg::LBRCommand::SharedPtr lbr_command_buffer_;
-  lbr_fri_msgs::msg::LBRState::SharedPtr lbr_state_buffer_;
+  lbr_fri_msgs::msg::LBRCommand lbr_command_buffer_;
+  lbr_fri_msgs::msg::LBRState lbr_state_buffer_;
 };
 } // end of namespace lbr_fri_ros2
 #endif // LBR_FRI_ROS2__LBR_INTERMEDIARY_HPP_
--- lbr_fri_ros2/src/lbr_intermediary.cpp
+++ lbr_fri_ros2/src/lbr_intermediary.cpp
@@ -1,123 +1,53 @@
 #include "lbr_fri_ros2/lbr_intermediary.hpp"
 
 namespace lbr_fri_ros2 {
-LBRIntermediary::LBRIntermediary() {
-  if (!reset_buffers_()) {
-    throw std::runtime_error("Failed to reset buffers.");
-  }
-}
-
-void LBRIntermediary::set_lbr_command_buffer_nan() {
-  lbr_command_buffer_->joint_position.fill(std::numeric_limits<double>::quiet_NaN());
-  lbr_command_buffer_->wrench.fill(std::numeric_limits<double>::quiet_NaN());
-  lbr_command_buffer_->torque.fill(std::numeric_limits<double>::quiet_NaN());
-}
-
-void LBRIntermediary::set_lbr_state_buffer_nan() {
-  lbr_state_buffer_->client_command_mode = std::numeric_limits<int8_t>::quiet_NaN();
-  lbr_state_buffer_->commanded_joint_position.fill(std::numeric_limits<double>::quiet_NaN());
-  lbr_state_buffer_->commanded_torque.fill(std::numeric_limits<double>::quiet_NaN());
-  lbr_state_buffer_->connection_quality = std::numeric_limits<int8_t>::quiet_NaN();
-  lbr_state_buffer_->control_mode = std::numeric_limits<int8_t>::quiet_NaN();
-  lbr_state_buffer_->drive_state = std::numeric_limits<int8_t>::quiet_NaN();
-  lbr_state_buffer_->external_torque.fill(std::numeric_limits<double>::quiet_NaN());
-  lbr_state_buffer_->ipo_joint_position.fill(std::numeric_limits<double>::quiet_NaN());
-  lbr_state_buffer_->measured_joint_position.fill(std::numeric_limits<double>::quiet_NaN());
-  lbr_state_buffer_->measured_torque.fill(std::numeric_limits<double>::quiet_NaN());
-  lbr_state_buffer_->operation_mode = std::numeric_limits<int8_t>::quiet_NaN();
-  lbr_state_buffer_->overlay_type = std::numeric_limits<int8_t>::quiet_NaN();
-  lbr_state_buffer_->safety_state = std::numeric_limits<int8_t>::quiet_NaN();
-  lbr_state_buffer_->sample_time = std::numeric_limits<double>::quiet_NaN();
-  lbr_state_buffer_->session_state = std::numeric_limits<int8_t>::quiet_NaN();
-  lbr_state_buffer_->time_stamp_nano_sec = std::numeric_limits<uint32_t>::quiet_NaN();
-  lbr_state_buffer_->time_stamp_sec = std::numeric_limits<uint32_t>::quiet_NaN();
-  lbr_state_buffer_->tracking_performance = std::numeric_limits<double>::quiet_NaN();
-}
-
-bool LBRIntermediary::reset_buffers_() {
-  if (!reset_lbr_command_buffer_()) {
-    return false;
-  }
-  if (!reset_lbr_state_buffer_()) {
-    return false;
-  }
-  return true;
-}
-
-bool LBRIntermediary::reset_lbr_command_buffer_() {
-  try {
-    lbr_command_buffer_ = std::make_shared<lbr_fri_msgs::msg::LBRCommand>();
-    set_lbr_command_buffer_nan();
-  } catch (const std::exception &e) {
-    printf("Failed to reset command buffer.\n%s", e.what());
-    return false;
-  }
-  return true;
-}
-
-bool LBRIntermediary::reset_lbr_state_buffer_() {
-  try {
-    lbr_state_buffer_ = std::make_shared<lbr_fri_msgs::msg::LBRState>();
-    set_lbr_state_buffer_nan();
-  } catch (const std::exception &e) {
-    printf("Failed to reset state buffer.\n%s", e.what());
-    return false;
-  }
-  return true;
-}
-
 bool LBRIntermediary::command_to_buffer(
     const lbr_fri_msgs::msg::LBRCommand::ConstSharedPtr lbr_command) {
-  if (lbr_command_is_nan_(lbr_command)) {
-    *lbr_command_buffer_ = *lbr_command;
-    return true;
+  if (!lbr_command) {
+    return false;
   }
-  return false;
+  lbr_command_buffer_ = *lbr_command;
+  return true;
 }
 
 bool LBRIntermediary::buffer_to_command(KUKA::FRI::LBRCommand &lbr_command) const {
   try {
-    if (lbr_command_is_nan_(lbr_command_buffer_)) {
-      switch (lbr_state_buffer_->client_command_mode) {
+    if (lbr_state_buffer_.client_command_mode == KUKA::FRI::ESessionState::COMMANDING_ACTIVE) {
+      switch (lbr_state_buffer_.client_command_mode) {
       case KUKA::FRI::EClientCommandMode::NO_COMMAND_MODE:
         return true;
       case KUKA::FRI::EClientCommandMode::POSITION:
-        lbr_command.setJointPosition(lbr_command_buffer_->joint_position.data());
+        lbr_command.setJointPosition(lbr_command_buffer_.joint_position.data());
         return true;
       case KUKA::FRI::EClientCommandMode::WRENCH:
-        lbr_command.setJointPosition(lbr_command_buffer_->joint_position.data());
-        lbr_command.setWrench(lbr_command_buffer_->wrench.data());
+        lbr_command.setJointPosition(lbr_command_buffer_.joint_position.data());
+        lbr_command.setWrench(lbr_command_buffer_.wrench.data());
         return true;
       case KUKA::FRI::EClientCommandMode::TORQUE:
-        lbr_command.setJointPosition(lbr_command_buffer_->joint_position.data());
-        lbr_command.setTorque(lbr_command_buffer_->torque.data());
+        lbr_command.setJointPosition(lbr_command_buffer_.joint_position.data());
+        lbr_command.setTorque(lbr_command_buffer_.torque.data());
         return true;
       default:
         printf("Unknown EClientCommandMode provided.\n");
         return false;
       }
     } else {
-      if (lbr_state_is_nan_(lbr_state_buffer_)) {
-        switch (lbr_state_buffer_->client_command_mode) {
-        case KUKA::FRI::EClientCommandMode::NO_COMMAND_MODE:
-          return true;
-        case KUKA::FRI::EClientCommandMode::POSITION:
-          lbr_command.setJointPosition(lbr_state_buffer_->measured_joint_position.data());
-          return true;
-        case KUKA::FRI::EClientCommandMode::WRENCH:
-          lbr_command.setJointPosition(lbr_state_buffer_->measured_joint_position.data());
-          lbr_command.setWrench(WRENCH_ZEROS.data());
-          return true;
-        case KUKA::FRI::EClientCommandMode::TORQUE:
-          lbr_command.setJointPosition(lbr_state_buffer_->measured_joint_position.data());
-          lbr_command.setTorque(JOINT_ZEROS.data());
-          return true;
-        default:
-          printf("Unknown EClientCommandMode provided.\n");
-          return false;
-        }
-      } else {
-        printf("Attempted to set command from nan state.\n");
+      switch (lbr_state_buffer_.client_command_mode) {
+      case KUKA::FRI::EClientCommandMode::NO_COMMAND_MODE:
+        return true;
+      case KUKA::FRI::EClientCommandMode::POSITION:
+        lbr_command.setJointPosition(lbr_state_buffer_.commanded_joint_position.data());
+        return true;
+      case KUKA::FRI::EClientCommandMode::WRENCH:
+        lbr_command.setJointPosition(lbr_state_buffer_.commanded_joint_position.data());
+        lbr_command.setWrench(WRENCH_ZEROS.data());
+        return true;
+      case KUKA::FRI::EClientCommandMode::TORQUE:
+        lbr_command.setJointPosition(lbr_state_buffer_.commanded_joint_position.data());
+        lbr_command.setTorque(JOINT_ZEROS.data());
+        return true;
+      default:
+        printf("Unknown EClientCommandMode provided.\n");
         return false;
       }
     }
@@ -129,41 +59,41 @@ bool LBRIntermediary::buffer_to_command(KUKA::FRI::LBRCommand &lbr_command) cons
 
 bool LBRIntermediary::state_to_buffer(const KUKA::FRI::LBRState &lbr_state) {
   try {
-    lbr_state_buffer_->client_command_mode = lbr_state.getClientCommandMode();
+    lbr_state_buffer_.client_command_mode = lbr_state.getClientCommandMode();
     auto commanded_joint_position = lbr_state.getCommandedJointPosition();
     std::copy(commanded_joint_position,
               commanded_joint_position + KUKA::FRI::LBRState::NUMBER_OF_JOINTS,
-              lbr_state_buffer_->commanded_joint_position.begin());
+              lbr_state_buffer_.commanded_joint_position.begin());
     auto commanded_torque = lbr_state.getCommandedTorque();
     std::copy(commanded_torque, commanded_torque + KUKA::FRI::LBRState::NUMBER_OF_JOINTS,
-              lbr_state_buffer_->commanded_torque.begin());
-    lbr_state_buffer_->connection_quality = lbr_state.getConnectionQuality();
-    lbr_state_buffer_->control_mode = lbr_state.getControlMode();
-    lbr_state_buffer_->drive_state = lbr_state.getDriveState();
+              lbr_state_buffer_.commanded_torque.begin());
+    lbr_state_buffer_.connection_quality = lbr_state.getConnectionQuality();
+    lbr_state_buffer_.control_mode = lbr_state.getControlMode();
+    lbr_state_buffer_.drive_state = lbr_state.getDriveState();
     auto external_torque = lbr_state.getExternalTorque();
     std::copy(external_torque, external_torque + KUKA::FRI::LBRState::NUMBER_OF_JOINTS,
-              lbr_state_buffer_->external_torque.begin());
+              lbr_state_buffer_.external_torque.begin());
     if (lbr_state.getSessionState() == KUKA::FRI::ESessionState::COMMANDING_WAIT ||
         lbr_state.getSessionState() == KUKA::FRI::ESessionState::COMMANDING_ACTIVE) {
       auto ipo_joint_position = lbr_state.getIpoJointPosition();
       std::copy(ipo_joint_position, ipo_joint_position + KUKA::FRI::LBRState::NUMBER_OF_JOINTS,
-                lbr_state_buffer_->ipo_joint_position.begin());
+                lbr_state_buffer_.ipo_joint_position.begin());
     }
     auto measured_joint_position = lbr_state.getMeasuredJointPosition();
     std::copy(measured_joint_position,
               measured_joint_position + KUKA::FRI::LBRState::NUMBER_OF_JOINTS,
-              lbr_state_buffer_->measured_joint_position.begin());
+              lbr_state_buffer_.measured_joint_position.begin());
     auto measured_torque = lbr_state.getMeasuredTorque();
     std::copy(measured_torque, measured_torque + KUKA::FRI::LBRState::NUMBER_OF_JOINTS,
-              lbr_state_buffer_->measured_torque.begin());
-    lbr_state_buffer_->operation_mode = lbr_state.getOperationMode();
-    lbr_state_buffer_->overlay_type = lbr_state.getOverlayType();
-    lbr_state_buffer_->safety_state = lbr_state.getSafetyState();
-    lbr_state_buffer_->sample_time = lbr_state.getSampleTime();
-    lbr_state_buffer_->session_state = lbr_state.getSessionState();
-    lbr_state_buffer_->time_stamp_nano_sec = lbr_state.getTimestampNanoSec();
-    lbr_state_buffer_->time_stamp_sec = lbr_state.getTimestampSec();
-    lbr_state_buffer_->tracking_performance = lbr_state.getTrackingPerformance();
+              lbr_state_buffer_.measured_torque.begin());
+    lbr_state_buffer_.operation_mode = lbr_state.getOperationMode();
+    lbr_state_buffer_.overlay_type = lbr_state.getOverlayType();
+    lbr_state_buffer_.safety_state = lbr_state.getSafetyState();
+    lbr_state_buffer_.sample_time = lbr_state.getSampleTime();
+    lbr_state_buffer_.session_state = lbr_state.getSessionState();
+    lbr_state_buffer_.time_stamp_nano_sec = lbr_state.getTimestampNanoSec();
+    lbr_state_buffer_.time_stamp_sec = lbr_state.getTimestampSec();
+    lbr_state_buffer_.tracking_performance = lbr_state.getTrackingPerformance();
   } catch (const std::exception &e) {
     printf("Failed to move state to buffer.\n%s", e.what());
     return false;
@@ -173,110 +103,9 @@ bool LBRIntermediary::state_to_buffer(const KUKA::FRI::LBRState &lbr_state) {
 
 bool LBRIntermediary::buffer_to_state(lbr_fri_msgs::msg::LBRState &lbr_state) const {
   try {
-    lbr_state = *lbr_state_buffer_;
+    lbr_state = lbr_state_buffer_;
   } catch (const std::exception &e) {
     printf("Failed to move buffer to state.\n%s", e.what());
-    return false;
-  }
-  return true;
-}
-
-bool LBRIntermediary::lbr_command_is_nan_(
-    const lbr_fri_msgs::msg::LBRCommand::ConstSharedPtr lbr_command) const {
-  if (!lbr_command) {
-    return false;
-  }
-
-  switch (lbr_state_buffer_->client_command_mode) {
-  case KUKA::FRI::EClientCommandMode::NO_COMMAND_MODE:
-    return true;
-  case KUKA::FRI::EClientCommandMode::POSITION:
-    if (!joint_position_command_is_nan_(lbr_command->joint_position)) {
-      return false;
-    }
-    break;
-  case KUKA::FRI::EClientCommandMode::WRENCH:
-    if (!joint_position_command_is_nan_(lbr_command->joint_position)) {
-      return false;
-    }
-    if (!wrench_command_is_nan_(lbr_command->wrench)) {
-      return false;
-    }
-    break;
-  case KUKA::FRI::EClientCommandMode::TORQUE:
-    if (!joint_position_command_is_nan_(lbr_command->joint_position)) {
-      return false;
-    }
-    if (!torque_command_is_nan_(lbr_command->torque)) {
-      return false;
-    }
-    break;
-  default:
-    printf("Unknown EClientCommandMode provided.\n");
-    return false;
-  }
-  return true;
-}
-
-bool LBRIntermediary::joint_position_command_is_nan_(
-    const JointArray &joint_position_command) const {
-  for (std::size_t i = 0; i < joint_position_command.size(); ++i) {
-    if (std::isnan(joint_position_command[i])) {
-      return false;
-    }
-  }
-  return true;
-}
-
-bool LBRIntermediary::wrench_command_is_nan_(const WrenchArray &wrench_command) const {
-  for (std::size_t i = 0; i < wrench_command.size(); ++i) {
-    if (std::isnan(wrench_command[i])) {
-      return false;
-    }
-  }
-  return true;
-}
-
-bool LBRIntermediary::torque_command_is_nan_(const JointArray &torque_command) const {
-  for (std::size_t i = 0; i < torque_command.size(); ++i) {
-    if (std::isnan(torque_command[i])) {
-      return false;
-    }
-  }
-  return true;
-}
-
-bool LBRIntermediary::lbr_state_is_nan_(
-    const lbr_fri_msgs::msg::LBRState::ConstSharedPtr lbr_state) const {
-  if (!lbr_state) {
-    printf("Found no state.\n");
-    return false;
-  }
-  for (std::size_t i = 0; i < lbr_state->commanded_joint_position.size(); ++i) {
-    if (std::isnan(lbr_state->commanded_joint_position[i]) ||
-        std::isnan(lbr_state->commanded_torque[i]) || std::isnan(lbr_state->external_torque[i]) ||
-        std::isnan(lbr_state->measured_joint_position[i]) ||
-        std::isnan(lbr_state->measured_torque[i])) {
-      printf("Found nan for state in joint %lu.\n", i);
-      return false;
-    }
-  }
-  if (lbr_state->session_state == KUKA::FRI::COMMANDING_WAIT ||
-      lbr_state->session_state == KUKA::FRI::COMMANDING_ACTIVE) {
-    for (std::size_t i = 0; i < lbr_state->ipo_joint_position.size(); ++i) {
-      if (std::isnan(lbr_state->ipo_joint_position[i])) {
-        printf("Found nan for interpolated joint position in joint %lu.\n", i);
-        return false;
-      }
-    }
-  }
-  if (std::isnan(lbr_state->client_command_mode) || std::isnan(lbr_state->connection_quality) ||
-      std::isnan(lbr_state->control_mode) || std::isnan(lbr_state->drive_state) ||
-      std::isnan(lbr_state->operation_mode) || std::isnan(lbr_state->overlay_type) ||
-      std::isnan(lbr_state->safety_state) || std::isnan(lbr_state->sample_time) ||
-      std::isnan(lbr_state->session_state) || std::isnan(lbr_state->time_stamp_nano_sec) ||
-      std::isnan(lbr_state->time_stamp_sec) || std::isnan(lbr_state->tracking_performance)) {
-    printf("Found nan in other state variables.\n");
     return false;
   }
   return true;
