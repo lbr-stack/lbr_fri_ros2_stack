8a421bf7dd8e98f3475f2f674b14fb1a17c48e95
mhubii <m.huber_1994@hotmail.de>
2023-04-26 12:56:37 +0100

    added basic fri examples

---
+++ lbr_demos/lbr_standalone_cpp_demos/src/joint_sine_overlay_node.cpp
@@ -0,0 +1,60 @@
+#include <functional>
+#include <math.h>
+#include <memory>
+#include <string>
+
+// include fri for session state
+#include "fri/friClientIf.h"
+
+#include "rclcpp/rclcpp.hpp"
+
+// include lbr_fri_msgs
+#include "lbr_fri_msgs/msg/lbr_command.hpp"
+#include "lbr_fri_msgs/msg/lbr_state.hpp"
+
+class JointSineOverlay : public rclcpp::Node {
+  static constexpr double amplitude_ = 0.04; // rad
+  static constexpr double frequency_ = 0.25; // Hz
+
+public:
+  JointSineOverlay(const std::string &node_name) : Node(node_name), phase_(0.) {
+    // create publisher to /lbr_command
+    lbr_command_pub_ = this->create_publisher<lbr_fri_msgs::msg::LBRCommand>(
+        "/lbr_command", rclcpp::SensorDataQoS());
+
+    // create subscription to /lbr_state
+    lbr_state_sub_ = this->create_subscription<lbr_fri_msgs::msg::LBRState>(
+        "/lbr_state", rclcpp::SensorDataQoS(),
+        std::bind(&JointSineOverlay::lbr_state_cb_, this, std::placeholders::_1));
+  };
+
+protected:
+  void lbr_state_cb_(const lbr_fri_msgs::msg::LBRState::SharedPtr lbr_state) {
+    lbr_command_.joint_position = lbr_state->ipo_joint_position;
+
+    if (lbr_state->session_state == KUKA::FRI::COMMANDING_ACTIVE) {
+      // overlay sine wave on 4th joint
+      lbr_command_.joint_position[3] += amplitude_ * sin(phase_);
+      phase_ += 2 * M_PI * frequency_ * lbr_state->sample_time;
+
+      lbr_command_pub_->publish(lbr_command_);
+    } else {
+      // reset phase
+      phase_ = 0.;
+    }
+  };
+
+  double phase_;
+
+  rclcpp::Publisher<lbr_fri_msgs::msg::LBRCommand>::SharedPtr lbr_command_pub_;
+  rclcpp::Subscription<lbr_fri_msgs::msg::LBRState>::SharedPtr lbr_state_sub_;
+
+  lbr_fri_msgs::msg::LBRCommand lbr_command_;
+};
+
+int main(int argc, char **argv) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<JointSineOverlay>("joint_sine_overlay_node"));
+  rclcpp::shutdown();
+  return 0;
+};
