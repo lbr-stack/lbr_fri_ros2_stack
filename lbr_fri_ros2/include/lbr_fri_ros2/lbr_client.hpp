81bd3d2ae1c947ce30a2948019d8cc0546ec3afc
mhubii <m.huber_1994@hotmail.de>
2023-06-19 13:35:58 +0100

    updated doc

--- lbr_fri_ros2/include/lbr_fri_ros2/lbr_client.hpp
+++ lbr_fri_ros2/include/lbr_fri_ros2/lbr_client.hpp
@@ -19,8 +19,8 @@
 
 namespace lbr_fri_ros2 {
 /**
- * @brief Simple implementation of KUKA::FRI::LBRClient. Has a shared node for reading commands from
- * / writing states to real-time safe topics.
+ * @brief Simple implementation of KUKA::FRI::LBRClient. Has a shared node for reading commands
+ * from / writing states to topics that follow the robot's QoS.
  *
  */
 class LBRClient : public KUKA::FRI::LBRClient {
@@ -30,8 +30,7 @@ public:
   /**
    * @brief Construct a new LBRClient object.
    *
-   * @param[in] node Shared node for reading commands from / writing states to real-time safe
-   * topics.
+   * @param[in] node Shared node for reading commands from / writing states to topics.
    * @param[in] lbr_command_guard Command guard for validating incoming commands.
    *
    */
@@ -53,15 +52,15 @@ public:
                      KUKA::FRI::ESessionState new_state) override;
 
   /**
-   * @brief Called when robot in KUKA::FRI::MONITORING_WAIT and
-   * KUKA::FRI::MONITORING_READY state. Publishes state from #robotState via #lbr_state_rt_pub_.
+   * @brief Called when robot in KUKA::FRI::MONITORING_WAIT or KUKA::FRI::MONITORING_READY state.
+   * Publishes state from #robotState via #lbr_state_pub_.
    *
    */
   void monitor() override;
 
   /**
    * @brief Called when robot in KUKA::FRI::COMMANDING_WAIT state. Publishes state from #robotState
-   * via #lbr_state_rt_pub_ and sets #robotCommand if in command mode KUKA::FRI::WRENCH or
+   * via #lbr_state_pub_ and sets #robotCommand if in command mode KUKA::FRI::WRENCH or
    * KUKA::FRI::TORQUE.
    *
    */
@@ -69,30 +68,66 @@ public:
 
   /**
    * @brief Called when robot in KUKA::FRI::COMMANDING_ACTIVE state. Publishes state from
-   * #robotState via #lbr_state_rt_pub_. Writes command from #lbr_command_rt_buf_ to #robotCommand.
+   * #robotState via #lbr_state_pub_. Writes command from #lbr_command_ to #robotCommand.
    *
    */
   void command() override;
 
 protected:
+  /**
+   * @brief Set the the #lbr_command_ to the current robot configuration. Sets commanded wrenches
+   * and torques to zero.
+   *
+   */
   void init_lbr_command_();
+
+  /**
+   * @brief Initialize topics, that is #lbr_command_sub_ and #lbr_state_pub_. This method may only
+   * be called after #robotState is accessible since QoS profiles utilize the robots's sample time.
+   *
+   */
   void init_topics_();
 
+  /**
+   * @brief Declare parameters for #node_ that are utilized within LBRClient.
+   *
+   */
   void declare_parameters_();
+
+  /**
+   * @brief Get parameters from #node_ and write them to members.
+   *
+   */
   void get_parameters_();
 
+  /**
+   * @brief Publish #lbr_state_ using #lbr_state_pub_.
+   *
+   */
   void pub_lbr_state_();
+
+  /**
+   * @brief Callback for #lbr_command_sub_. Validates incoming commands using #lbr_command_guard_
+   * prior to writing them into #lbr_command_.
+   *
+   * @param[in] lbr_command
+   */
   void on_lbr_command_(const lbr_fri_msgs::msg::LBRCommand::SharedPtr lbr_command);
 
   rclcpp::Node::SharedPtr node_; /**< Shared pointer to node.*/
 
-  uint32_t missed_deadlines_pub_, missed_deadlines_sub_;
+  uint32_t missed_deadlines_pub_; /**< Counter for states that were not published within specified
+                                     deadline.*/
+  uint32_t missed_deadlines_sub_; /**< Counter for commands that were not received within specified
+                                     deadline.*/
 
-  lbr_fri_msgs::msg::LBRCommand lbr_command_;
-  lbr_fri_msgs::msg::LBRState lbr_state_;
+  lbr_fri_msgs::msg::LBRCommand lbr_command_; /**< Command buffer.*/
+  lbr_fri_msgs::msg::LBRState lbr_state_;     /**< State buffer.*/
 
-  std::string lbr_command_topic_, lbr_state_topic_;
-  double smoothing_;
+  std::string
+      lbr_command_topic_; /**< Command topic to be subscribed from, defaults to /lbr_command.*/
+  std::string lbr_state_topic_; /**< State topic to be published to, defaults to /lbr_state.*/
+  double smoothing_;            /**< Exponential smoothing factor for position commands.*/
 
   std::unique_ptr<LBRCommandGuard>
       lbr_command_guard_; /**< Validating commands prior to writing them to #robotCommand.*/
