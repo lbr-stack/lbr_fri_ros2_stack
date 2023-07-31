9418fe167403485033da6daaf2ffceb0a2d24e85
mhubii <m.huber_1994@hotmail.de>
2023-07-31 13:58:28 +0100

    added a robot description factory

---
+++ lbr_description/lbr_description/__init__.py
@@ -0,0 +1,1 @@
+from .description_factory import description_factory
---
+++ lbr_description/lbr_description/description_factory.py
@@ -0,0 +1,34 @@
+import os
+from typing import Dict
+
+import xacro
+from ament_index_python import get_package_share_directory
+
+
+def description_factory(
+    model: str = "iiwa7", robot_name: str = "lbr", sim: bool = True
+) -> Dict[str, str]:
+    r"""Robot description factory for the LBR.
+
+    Args:
+        model (str): The LBR model in use. Defaults to "iiwa7". Choices are "iiwa7", "iiwa14", "med7", "med14".
+        robot_name  (str): The name of the robot.
+        sim (bool): Whether to use the simulation or not.
+
+    Returns:
+        robot_description (Dict[str, str]): The robot description as a dictionary.
+    """
+    robot_description = {
+        "robot_description": xacro.process(
+            os.path.join(
+                get_package_share_directory("lbr_description"),
+                f"urdf/{model}/{model}.urdf.xacro",
+            ),
+            mappings={
+                "robot_name": robot_name,
+                "sim": "true" if sim else "false",
+            },
+        )
+    }
+
+    return robot_description
