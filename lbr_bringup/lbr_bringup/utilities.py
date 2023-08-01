dc010ca196edf9f6b40ef792c878588e0f115b9e
mhubii <m.huber_1994@hotmail.de>
2023-08-01 19:39:01 +0100

    removed legacy bringup

--- lbr_bringup/lbr_bringup/utilities.py
+++
@@ -1,9 +0,0 @@
-def string_to_bool(string: str = "true"):
-    if string in ["True", "true"]:
-        return True
-    elif string in ["False", "false"]:
-        return False
-    else:
-        raise ValueError(
-            f"String must be one of ['True', 'true', 'False', 'false'], got {string}."
-        )
