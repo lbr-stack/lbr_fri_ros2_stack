7ded28b3f30eb0c332e129a919d9b626897ea58a
mhubii <m.huber_1994@hotmail.de>
2023-08-05 14:08:28 +0100

    added open loop param

--- lbr_fri_ros2/launch/lbr_app.launch.py
+++ lbr_fri_ros2/launch/lbr_app.launch.py
@@ -1,7 +1,7 @@
 from launch import LaunchDescription
 
-from lbr_fri_ros2 import LBRFRIROS2Mixin
 from lbr_description import LBRDescriptionMixin
+from lbr_fri_ros2 import LBRFRIROS2Mixin
 
 
 def generate_launch_description() -> LaunchDescription:
@@ -10,11 +10,13 @@ def generate_launch_description() -> LaunchDescription:
     ld.add_action(LBRDescriptionMixin.arg_model())
     ld.add_action(LBRDescriptionMixin.arg_robot_name())
     robot_description = LBRDescriptionMixin.param_robot_description(sim=False)
+    ld.add_action(LBRFRIROS2Mixin.arg_open_loop())
     ld.add_action(
         LBRFRIROS2Mixin.node_lbr_app(
             parameters=[
                 robot_description,
                 LBRDescriptionMixin.param_robot_name(),
+                LBRFRIROS2Mixin.param_open_loop(),
             ]
         )
     )
--- lbr_fri_ros2/lbr_fri_ros2/launch_mixin.py
+++ lbr_fri_ros2/lbr_fri_ros2/launch_mixin.py
@@ -16,12 +16,24 @@ class LBRFRIROS2Mixin:
         )
 
     @staticmethod
+    def arg_open_loop() -> DeclareLaunchArgument:
+        return DeclareLaunchArgument(
+            name="open_loop",
+            default_value="true",
+            description="Open loop control. Works best for LBRs. Should only be set to false by experiences users.",
+        )
+
+    @staticmethod
     def param_command_guard_variant() -> Dict[str, LaunchConfiguration]:
         return {
             "command_guard_variant": LaunchConfiguration(
                 "command_guard_variant", default="safe_stop"
             )
         }
+
+    @staticmethod
+    def param_open_loop() -> Dict[str, LaunchConfiguration]:
+        return {"open_loop": LaunchConfiguration("open_loop", default="true")}
 
     @staticmethod
     def node_lbr_app(**kwargs) -> DeclareLaunchArgument:
