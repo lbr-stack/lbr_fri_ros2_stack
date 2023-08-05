ef929ffaba315b8131545f3a5d28175b6eb46ac1
mhubii <m.huber_1994@hotmail.de>
2023-08-05 14:09:07 +0100

    1.3.0 -> 1.2.2

--- lbr_demos/lbr_demos_ros2_control_python/setup.py
+++ lbr_demos/lbr_demos_ros2_control_python/setup.py
@@ -4,7 +4,7 @@ package_name = "lbr_demos_ros2_control_python"
 
 setup(
     name=package_name,
-    version="1.3.0",
+    version="1.2.2",
     packages=[package_name],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
