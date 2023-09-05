6afe68b1fe0b7b27ae14e317602cbc79d3b394b6
mhubii <m.huber_1994@hotmail.de>
2023-09-05 17:37:10 +0100

    1.2.4 -> 1.2.5

--- lbr_demos/lbr_demos_fri_ros2_python/setup.py
+++ lbr_demos/lbr_demos_fri_ros2_python/setup.py
@@ -6,7 +6,7 @@ package_name = "lbr_demos_fri_ros2_python"
 
 setup(
     name=package_name,
-    version="1.2.4",
+    version="1.2.5",
     packages=[package_name],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
