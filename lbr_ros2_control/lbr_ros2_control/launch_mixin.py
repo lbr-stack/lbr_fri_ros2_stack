231859fc33e1bf98ef6caf52d6c1665d121884cc
mhubii <m.huber_1994@hotmail.de>
2023-12-05 20:18:19 +0000

    added choices to launch

--- lbr_ros2_control/lbr_ros2_control/launch_mixin.py
+++ lbr_ros2_control/lbr_ros2_control/launch_mixin.py
@@ -29,7 +29,12 @@ class LBRROS2ControlMixin:
             name="ctrl",
             default_value="position_trajectory_controller",
             description="Desired default controller. One of specified in ctrl_cfg.",
-            choices=["position_trajectory_controller", "forward_position_controller"],
+            choices=[
+                "position_trajectory_controller",
+                "forward_position_controller",
+                "forward_lbr_position_command_controller",
+                "forward_lbr_torque_command_controller",
+            ],
         )
 
     @staticmethod
