7caf32ef446718fe9c796342b4d67cb43a4a1ef6
mhubii <m.huber_1994@hotmail.de>
2023-12-15 17:07:49 +0000

    bumped version 1.4.0 -> 1.4.1

--- lbr_demos/lbr_demos_fri_ros2_advanced_python/setup.py
+++ lbr_demos/lbr_demos_fri_ros2_advanced_python/setup.py
@@ -6,7 +6,7 @@ package_name = "lbr_demos_fri_ros2_advanced_python"
 
 setup(
     name=package_name,
-    version="1.4.0",
+    version="1.4.1",
     packages=[package_name],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
