1dd97f728af53e0bedd9a4ca4d4fc443751f23d5
mhubii <m.huber_1994@hotmail.de>
2024-03-13 11:48:58 +0000

    formatted https://github.com/lbr-stack/lbr_fri_ros2_stack/pull/147

--- lbr_moveit_config/med14_moveit_config/launch/setup_assistant.launch.py
+++ lbr_moveit_config/med14_moveit_config/launch/setup_assistant.launch.py
@@ -3,5 +3,7 @@ from moveit_configs_utils.launches import generate_setup_assistant_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("med14", package_name="med14_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder(
+        "med14", package_name="med14_moveit_config"
+    ).to_moveit_configs()
     return generate_setup_assistant_launch(moveit_config)
