ed18417a80bed282043a04781cd447166b4989f3
cmower <christopher.mower@kcl.ac.uk>
2024-05-04 16:25:17 +0200

    simplified base node

--- lbr_demos/lbr_demos_fri_ros2_advanced_python/lbr_demos_fri_ros2_advanced_python/lbr_base_position_command_node.py
+++ lbr_demos/lbr_demos_fri_ros2_advanced_python/lbr_demos_fri_ros2_advanced_python/lbr_base_position_command_node.py
@@ -1,4 +1,6 @@
 import rclpy
+import rclpy.parameter
+from rcl_interfaces.msg import ParameterValue
 from rcl_interfaces.srv import GetParameters
 from rclpy.node import Node
 
@@ -18,9 +20,13 @@ class LBRBasePositionCommandNode(Node):
         super().__init__(node_name)
 
         # retrieve parameters
-        self._update_rate = self._get_update_rate()
+        self._robot_description = self._retrieve_parameter(
+            "robot_state_publisher/get_parameters", "robot_description"
+        ).string_value
+        self._update_rate = self._retrieve_parameter(
+            "controller_manager/get_parameters", "update_rate"
+        ).integer_value
         self._dt = 1.0 / float(self._update_rate)
-        self._robot_description = self._get_robot_description()
 
         # publishers and subscribers
         self._lbr_state_sub = self.create_subscription(
@@ -32,57 +38,23 @@ class LBRBasePositionCommandNode(Node):
             1,
         )
 
-    def _get_update_rate(self) -> int:
-        update_rate_client = self.create_client(
-            GetParameters, "controller_manager/get_parameters"
-        )
-        while not update_rate_client.wait_for_service(timeout_sec=1.0):
+    def _retrieve_parameter(self, service: str, parameter_name: str) -> ParameterValue:
+        parameter_client = self.create_client(GetParameters, service)
+        while not parameter_client.wait_for_service(timeout_sec=1.0):
             if not rclpy.ok():
                 self.get_logger().error(
                     "Interrupted while waiting for the service. Exiting."
                 )
                 return None
-            self.get_logger().info(
-                f"Waiting for {update_rate_client.srv_name} service..."
-            )
-        future = update_rate_client.call_async(
-            GetParameters.Request(names=["update_rate"])
-        )
+            self.get_logger().info(f"Waiting for '{service}' service...")
+        request = GetParameters.Request(names=[parameter_name])
+        future = parameter_client.call_async(request=request)
         rclpy.spin_until_future_complete(self, future)
         if future.result() is None:
-            self.get_logger().error("Service call failed!")
+            self.get_logger().error(f"Failed to retrieve '{parameter_name}'.")
             return None
-        self._update_rate = future.result().values[0].integer_value
-        self.get_logger().info(
-            f"Received update rate: {self._update_rate} Hz from {update_rate_client.srv_name}"
-        )
-        return self._update_rate
-
-    def _get_robot_description(self) -> str:
-        robot_description_client = self.create_client(
-            GetParameters, "robot_state_publisher/get_parameters"
-        )
-        while not robot_description_client.wait_for_service(timeout_sec=1.0):
-            if not rclpy.ok():
-                self.get_logger().error(
-                    "Interrupted while waiting for the service. Exiting."
-                )
-                return None
-            self.get_logger().info(
-                f"Waiting for {robot_description_client.srv_name} service..."
-            )
-        future = robot_description_client.call_async(
-            GetParameters.Request(names=["robot_description"])
-        )
-        rclpy.spin_until_future_complete(self, future)
-        if future.result() is None:
-            self.get_logger().error("Service call failed!")
-            return None
-        robot_description = future.result().values[0].string_value
-        self.get_logger().info(
-            f"Received robot description from {robot_description_client.srv_name}"
-        )
-        return robot_description
+        self.get_logger().info(f"Received '{parameter_name}' from '{service}'.")
+        return future.result().values[0]
 
     def _on_lbr_state(self, lbr_state: LBRState) -> None:
         raise NotImplementedError
