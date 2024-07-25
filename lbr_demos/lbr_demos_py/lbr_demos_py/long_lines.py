9d5309245d974656f74c61cf94b12e926560db11
Omid <omid.rezayof@utexas.edu>
2024-07-25 11:34:13 -0500

    Needle height adjustment after pivot calibration, adding bioprinter ROS connection (publisher)

--- lbr_demos/lbr_demos_py/lbr_demos_py/long_lines.py
+++ lbr_demos/lbr_demos_py/lbr_demos_py/long_lines.py
@@ -8,6 +8,8 @@ import numpy as np
 import threading
 from lbr_demos_py.asbr import * 
 from time import sleep
+import os
+
 
 
 
@@ -15,7 +17,7 @@ first_timestamp = None
 
 def listen_for_enter():
     global entered
-    input("Press Enter to record time stamp:")
+    input("Press Enter to record time stamp: \n")
     entered = True
 
     
@@ -47,15 +49,30 @@ class PrintLines(Node):
 
         self.goal_state = False
         self.commiunication_rate = 0.01
-        self.printer_pub = self.create_publisher(Float32, 'syringeVel', 1) 
+        self.printer_pub = self.create_publisher(Float32, '/syringevel', 1) 
         self.curr_pose = Pose()
 
     def curr_pose_update(self, msg):
         self.curr_pose = msg
 
     
-    def send_request(self, goal_pose, lin_vel):
-        self.request.goal_pose = goal_pose
+    def send_request_frame(self, goal_pose, lin_vel, frame = 'Needle'): #Needle or EE
+        if(frame == 'Needle'):
+            T_rn = Transformation(goal_pose) #T needle in robot base frame
+            T_ne = Pose() #EE in Needle transformation
+            T_ne.orientation = (Rotation()).as_geometry_orientation()
+            T_ne.position.x = -0.0200
+            T_ne.position.y = 0.0011
+            T_ne.position.z = -0.2031
+            T_ne = Transformation(T_ne)
+            T_re = T_rn*T_ne
+            self.request.goal_pose = T_re.as_geometry_pose()
+        elif(frame == 'EE'):
+            self.request.goal_pose = goal_pose
+        else:
+            print('ERROR: Send request in unknown frame.')
+            return False
+
         self.request.lin_vel = Float32(data = lin_vel)
         self.future = self.move_client.call_async(self.request)
         rclpy.spin_until_future_complete(self, self.future)
@@ -72,54 +89,63 @@ class PrintLines(Node):
         return 
 
     def go_home(self, home_pose, lin_vel = 0.005):
-        response = self.send_request(home_pose, lin_vel)
+        response = self.send_request_frame(home_pose, lin_vel, 'EE')
         print(response)
         self.wait_for_goal()    
         return 
 
 
 
-    def print_lines(self, start_pos, z_table_height, z_surface_height, needle_surface_dist_ideal, lin_vel):
+    def print_lines(self, start_pos, lin_vel):
         global entered
         entered = False
-        
+
+
+        needle_surface_dist_ideal = 1.5 * 0.001
+        z_table_height = 3 * 0.001 #Wooden table height, found from divot location after pivot calibration
+        sheet_height = 3.3 * 0.001 #White sheet thickness
+        needle_height = needle_surface_dist_ideal + z_table_height + sheet_height
+
         line_distance = 50 * 0.001
-        print_length = 50 * 0.001
-        line_length = 100 * 0.001
-        num_lines = 2
+        print_length = 130 * 0.001
+        line_length = 280 * 0.001
+        num_lines = 1
         direction = 1
         needle_position_file_counter = 0
-        print_vel = 0.25 #TODO
+        print_vel = 0.8 #TODO
+        file_name = "1_time_stamps.txt"
         
-        file = open("time_stamps.txt", "w")
-
+        if os.path.exists(file_name):
+            a = input('A file exists with similar name. Are you sure you want to proceed?')
+        file = open(file_name, "w")
         file.write('Settings:\n')
         file.write('line_distance: ' + str(line_distance) + '\n')
         file.write('print_length: ' + str(print_length) + '\n')
         file.write('line_length: ' + str(line_length) + '\n')
         file.write('print_vel: ' + str(print_vel) + '\n')
         file.write('z_table_height: ' + str(z_table_height) + '\n')
-        file.write('z_surface_height: ' + str(z_surface_height) + '\n')
+        file.write('sheet_height: ' + str(sheet_height) + '\n')
         file.write('needle_surface_dist_ideal: ' + str(needle_surface_dist_ideal) + '\n')
+        file.write('needle_height: ' + str(needle_height) + '\n')
         file.write('\nTime Stamps: \n \n')
 
-        needle_pos_time = np.array([self.curr_pose.position.x,self.curr_pose.position.y,self.curr_pose.position.z,create_relative_timestamp()])
+        curr_needle_pos = self.needle_loc()
+        needle_pos_time = np.array([curr_needle_pos.position.x,curr_needle_pos.position.y,curr_needle_pos.position.z,create_relative_timestamp()])
 
         thread = threading.Thread(target=listen_for_enter)
         thread.daemon = True
         thread.start()
-        # needle_height = z_surface_height + z_table_height + needle_surface_dist_ideal
-        needle_height = start_pos.position.z - 0.02
+        
 
 
         for line_num in range(num_lines):
-            print('line_num: ', line_num)
+            # print('line_num: ', line_num)
             file.write('\nLine number: ' + str(line_num+1) + ' out of ' + str(num_lines) + '\n\n')
             line_x = start_pos.position.x + line_distance * line_num
             line_start_y = start_pos.position.y + (-line_length if direction == -1 else 0)
             line_end_y = start_pos.position.y + (-line_length if direction == 1 else 0)
             print_end_y = line_start_y + (-print_length if direction == 1 else print_length)
-            print(line_end_y,print_end_y)
+            # print(line_end_y,print_end_y)
 
             print_start_pose = Pose()
             print_start_pose.position.x = line_x
@@ -140,48 +166,51 @@ class PrintLines(Node):
             line_end_pose.orientation = (Rotation.from_ABC([180,0,180],True)).as_geometry_orientation()
 
             # self.goal_state = False
-            response = self.send_request(print_start_pose, lin_vel)
+            response = self.send_request_frame(print_start_pose, lin_vel)
             self.wait_for_goal()
+            a= input('Do you want to proceed?')
 
 
             sleep(0.2)
             file.write('Starting at time: ' + str(create_relative_timestamp()) + '\nline_x: ' + str(line_x) + '\nline_start_y: ' + str(line_start_y) + '\nprint_end_y: ' + str(print_end_y) + '\nline_end_y: ' + str(line_end_y) + '\n')
-            file.write('Actual position: x: ' + str(self.curr_pose.position.x) + ' y: ' + str(self.curr_pose.position.y) + ' z: ' + str(self.curr_pose.position.z) + '\n')
+            curr_needle_pos = self.needle_loc()
+            file.write('Actual needle position: x: ' + str(curr_needle_pos.position.x) + ' y: ' + str(curr_needle_pos.position.y) + ' z: ' + str(curr_needle_pos.position.z) + '\n')
             file.write('\n')
 
             self.printer_pub.publish(Float32(data=print_vel))  # TODO
             # self.goal_state = False
-            response = self.send_request(print_end_pose, lin_vel)
+            response = self.send_request_frame(line_end_pose, lin_vel)
 
             while(not self.goal_state):
                 if (needle_position_file_counter%10==0):
                     needle_pos_time = np.vstack((needle_pos_time,np.asarray([self.curr_pose.position.x,self.curr_pose.position.y,self.curr_pose.position.z,create_relative_timestamp()])))
                 if(entered):
                     file.write('Time stamp per user request: ' + str(create_relative_timestamp()) + '\n')
-                    file.write('Actual position: x: ' + str(self.curr_pose.position.x) + ' y: ' + str(self.curr_pose.position.y) + ' z: ' + str(self.curr_pose.position.z) + '\n')
+                    curr_needle_pos = self.needle_loc()
+                    file.write('Actual needle position: x: ' + str(curr_needle_pos.position.x) + ' y: ' + str(curr_needle_pos.position.y) + ' z: ' + str(curr_needle_pos.position.z) + '\n')
                     file.write('\n')
                     entered = False
                     thread = threading.Thread(target=listen_for_enter)
                     thread.daemon = True
                     thread.start()
+
+                if(self.is_close_pos(self.needle_loc(), print_end_pose)):
+                    file.write('Injection stopped at time: ' + str(create_relative_timestamp()) + '\n')
+                    curr_needle_pos = self.needle_loc()
+                    file.write('Actual needle position: x: ' + str(curr_needle_pos.position.x) + ' y: ' + str(curr_needle_pos.position.y) + ' z: ' + str(curr_needle_pos.position.z) + '\n')
+                    file.write('\n')
+                    self.printer_pub.publish(Float32(data = 0.0))
                 
                 needle_position_file_counter = needle_position_file_counter + 1
                 rclpy.spin_once(self, timeout_sec = self.commiunication_rate)
             self.goal_state = False
 
-
-            file.write('Injection stopped at time: ' + str(create_relative_timestamp()) + '\n')
-            file.write('Actual position: x: ' + str(self.curr_pose.position.x) + ' y: ' + str(self.curr_pose.position.y) + ' z: ' + str(self.curr_pose.position.z) + '\n')
-            file.write('\n')
-            self.printer_pub.publish(Float32(data = 0.0))
-
-            response = self.send_request(line_end_pose, lin_vel)
-            self.wait_for_goal()
-
             file.write('End line reached at time: ' + str(create_relative_timestamp()) + '\n')
-            file.write('Actual position: x: ' + str(self.curr_pose.position.x) + ' y: ' + str(self.curr_pose.position.y) + ' z: ' + str(self.curr_pose.position.z) + '\n')
+            curr_needle_pos = self.needle_loc()
+            file.write('Actual needle position: x: ' + str(curr_needle_pos.position.x) + ' y: ' + str(curr_needle_pos.position.y) + ' z: ' + str(curr_needle_pos.position.z) + '\n')
             file.write('\n')
             sleep(1)
+            a= input('Do you want to proceed?')
             direction = -direction
         sleep(0.1)
         file.close()
@@ -190,37 +219,58 @@ class PrintLines(Node):
         
         print('Take me to shooting pose!')
         shooting_pose = Pose()
-        shooting_pose.position.x = 0.550
+        shooting_pose.position.x = 0.60
         shooting_pose.position.y = 0.0
-        shooting_pose.position.z = 0.50
+        shooting_pose.position.z = 0.22
         shooting_pose.orientation = (Rotation.from_ABC([180,0,180],True)).as_geometry_orientation()
 
-        response = self.send_request(shooting_pose, lin_vel)
+        response = self.send_request_frame(shooting_pose, lin_vel*2, 'EE')
         self.wait_for_goal()
         
 
 
-        
-        needle_pos_time = np.vstack((needle_pos_time,np.asarray([self.curr_pose.position.x,self.curr_pose.position.y,self.curr_pose.position.z,create_relative_timestamp()])))
-        np.save('needle_array_data.npy', needle_pos_time)
+        curr_needle_pos = self.needle_loc()
+        needle_pos_time = np.vstack((needle_pos_time,np.asarray([curr_needle_pos.position.x,curr_needle_pos.position.y,curr_needle_pos.position.z,create_relative_timestamp()])))
+        np.save('2_needle_array_data.npy', needle_pos_time)
         sleep(2)
+
+    def is_close_pos(self, Pose1, Pose2, pos_thresh=0.0005):
+        if(pos_thresh < 0.00001):
+            pos_thresh = 0.00001
+        translation_vec = np.asarray([Pose1.position.x - Pose2.position.x,
+                                      Pose1.position.y - Pose2.position.y,
+                                      Pose1.position.z - Pose2.position.z])
+        return np.linalg.norm(translation_vec) < pos_thresh
+
+    def needle_loc (self):
+        T_rn = Transformation() #T needle in robot base frame)
+        T_en = Pose() #EE in Needle transformation
+        T_en.orientation = (Rotation()).as_geometry_orientation()
+        T_en.position.x = 0.0200
+        T_en.position.y = -0.0011
+        T_en.position.z = 0.2031
+        T_en = Transformation(T_en)
+        T_re = Transformation(self.curr_pose)
+        T_rn = T_re*T_en
+        return T_rn.as_geometry_pose()
+
 
 
 def main(args=None):
     rclpy.init(args=args)
     node = PrintLines()
-    lin_vel = 0.005
+    lin_vel = 0.003
     home_pose = Pose()
     home_pose.position.x = 0.6
-    home_pose.position.y = 0.05
+    home_pose.position.y = 0.14
     home_pose.position.z = 0.45
     home_pose.orientation = (Rotation.from_ABC([180,0,180],True)).as_geometry_orientation()
 
-    node.go_home(home_pose, lin_vel)
+    node.go_home(home_pose, 0.02)
     print('Home position finished')
     sleep(2)
 
-    node.print_lines(home_pose, 0.01, 0.02, 0.4, 0.005)
+    node.print_lines(home_pose, lin_vel)
     node.destroy_node()
     rclpy.shutdown()
 
