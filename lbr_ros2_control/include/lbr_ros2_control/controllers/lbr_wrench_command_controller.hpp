7191199aa1e0c38792d671ffd648ac2027a6d80a
mhubii <m.huber_1994@hotmail.de>
2025-03-31 14:40:38 +0100

    added a template for chainable wrench controller (#250)

--- lbr_ros2_control/include/lbr_ros2_control/controllers/lbr_wrench_command_controller.hpp
+++ lbr_ros2_control/include/lbr_ros2_control/controllers/lbr_wrench_command_controller.hpp
@@ -8,8 +8,11 @@
 #include <string>
 #include <vector>
 
+#include "controller_interface/chainable_controller_interface.hpp"
 #include "controller_interface/controller_interface.hpp"
+#include "geometry_msgs/msg/wrench.hpp"
 #include "hardware_interface/loaned_command_interface.hpp"
+#include "hardware_interface/loaned_state_interface.hpp"
 #include "hardware_interface/types/hardware_interface_type_values.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "realtime_tools/realtime_buffer.hpp"
@@ -21,7 +24,7 @@
 #include "lbr_ros2_control/system_interface_type_values.hpp"
 
 namespace lbr_ros2_control {
-class LBRWrenchCommandController : public controller_interface::ControllerInterface {
+class LBRWrenchCommandController : public controller_interface::ChainableControllerInterface {
   static constexpr uint8_t CARTESIAN_DOF = 6;
 
 public:
@@ -33,8 +36,19 @@ public:
 
   controller_interface::CallbackReturn on_init() override;
 
-  controller_interface::return_type update(const rclcpp::Time &time,
-                                           const rclcpp::Duration &period) override;
+protected:
+  std::vector<hardware_interface::StateInterface> on_export_state_interfaces() override;
+  std::vector<hardware_interface::CommandInterface> on_export_reference_interfaces() override;
+  bool on_set_chained_mode(bool chained_mode) override;
+
+  // expect full lbr_wrench command in this mode....
+  controller_interface::return_type
+  update_reference_from_subscribers(const rclcpp::Time &time,
+                                    const rclcpp::Duration &period) override;
+
+  // expect just wrench command in this mode....
+  controller_interface::return_type
+  update_and_write_commands(const rclcpp::Time &time, const rclcpp::Duration &period) override;
 
   controller_interface::CallbackReturn
   on_configure(const rclcpp_lifecycle::State &previous_state) override;
@@ -46,19 +60,35 @@ public:
   on_deactivate(const rclcpp_lifecycle::State &previous_state) override;
 
 protected:
+  bool reference_state_interfaces_();
   bool reference_command_interfaces_();
+  void clear_state_interfaces_();
   void clear_command_interfaces_();
   void configure_joint_names_();
+  void init_lbr_wrench_command_subscription_();
+  void init_wrench_command_subscription_();
+  void reset_lbr_wrench_command_subscription_();
+  void reset_wrench_command_subscription_();
 
   lbr_fri_ros2::jnt_name_array_t joint_names_;
 
+  // state interfaces
+  std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>>
+      joint_position_state_interfaces_, joint_velocity_state_interfaces_;
+
+  // command interfaces
   std::vector<std::reference_wrapper<hardware_interface::LoanedCommandInterface>>
       joint_position_command_interfaces_, wrench_command_interfaces_;
 
+  // in external mode, wrench and joint position are commanded
   realtime_tools::RealtimeBuffer<lbr_fri_idl::msg::LBRWrenchCommand::SharedPtr>
       rt_lbr_wrench_command_ptr_;
   rclcpp::Subscription<lbr_fri_idl::msg::LBRWrenchCommand>::SharedPtr
       lbr_wrench_command_subscription_ptr_;
+
+  // in chained mode, only wrench is commanded
+  realtime_tools::RealtimeBuffer<geometry_msgs::msg::Wrench::SharedPtr> rt_wrench_command_ptr_;
+  rclcpp::Subscription<geometry_msgs::msg::Wrench>::SharedPtr wrench_command_subscription_ptr_;
 };
 } // namespace lbr_ros2_control
 #endif // LBR_ROS2_CONTROL__LBR_WRENCH_COMMAND_CONTROLLER_HPP_
--- lbr_ros2_control/src/controllers/lbr_wrench_command_controller.cpp
+++ lbr_ros2_control/src/controllers/lbr_wrench_command_controller.cpp
@@ -2,7 +2,8 @@
 
 namespace lbr_ros2_control {
 LBRWrenchCommandController::LBRWrenchCommandController()
-    : rt_lbr_wrench_command_ptr_(nullptr), lbr_wrench_command_subscription_ptr_(nullptr) {}
+    : rt_lbr_wrench_command_ptr_(nullptr), lbr_wrench_command_subscription_ptr_(nullptr),
+      rt_wrench_command_ptr_(nullptr), wrench_command_subscription_ptr_(nullptr) {}
 
 controller_interface::InterfaceConfiguration
 LBRWrenchCommandController::command_interface_configuration() const {
@@ -22,17 +23,18 @@ LBRWrenchCommandController::command_interface_configuration() const {
 
 controller_interface::InterfaceConfiguration
 LBRWrenchCommandController::state_interface_configuration() const {
-  return controller_interface::InterfaceConfiguration{
-      controller_interface::interface_configuration_type::NONE};
+  controller_interface::InterfaceConfiguration interface_configuration;
+  interface_configuration.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+  for (const auto &joint_name : joint_names_) {
+    interface_configuration.names.push_back(joint_name + "/" + hardware_interface::HW_IF_POSITION);
+    interface_configuration.names.push_back(joint_name + "/" + hardware_interface::HW_IF_VELOCITY);
+  }
+  return interface_configuration;
 }
 
 controller_interface::CallbackReturn LBRWrenchCommandController::on_init() {
   try {
-    lbr_wrench_command_subscription_ptr_ =
-        this->get_node()->create_subscription<lbr_fri_idl::msg::LBRWrenchCommand>(
-            "command/wrench", 1, [this](const lbr_fri_idl::msg::LBRWrenchCommand::SharedPtr msg) {
-              rt_lbr_wrench_command_ptr_.writeFromNonRT(msg);
-            });
+    init_lbr_wrench_command_subscription_();
     this->get_node()->declare_parameter("robot_name", "lbr");
     configure_joint_names_();
   } catch (const std::exception &e) {
@@ -44,20 +46,57 @@ controller_interface::CallbackReturn LBRWrenchCommandController::on_init() {
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
+std::vector<hardware_interface::StateInterface>
+LBRWrenchCommandController::on_export_state_interfaces() {
+  std::vector<hardware_interface::StateInterface> state_interfaces;
+  return state_interfaces;
+}
+
+std::vector<hardware_interface::CommandInterface>
+LBRWrenchCommandController::on_export_reference_interfaces() {
+  std::vector<hardware_interface::CommandInterface> command_interfaces;
+  return command_interfaces; // allow other controllers to write join positions.... i.e. write to
+                             // joint_position_command_interfaces_
+}
+
+bool LBRWrenchCommandController::on_set_chained_mode(bool chained_mode) {
+  try {
+    if (chained_mode) {
+      reset_lbr_wrench_command_subscription_();
+      init_wrench_command_subscription_();
+    } else {
+      reset_wrench_command_subscription_();
+      init_lbr_wrench_command_subscription_();
+    }
+  } catch (const std::exception &e) {
+    RCLCPP_ERROR(this->get_node()->get_logger(), "Failed to switch to chained mode with: %s.",
+                 e.what());
+    return false;
+  }
+  return true;
+}
+
 controller_interface::return_type
-LBRWrenchCommandController::update(const rclcpp::Time & /*time*/,
-                                   const rclcpp::Duration & /*period*/) {
+LBRWrenchCommandController::update_reference_from_subscribers(const rclcpp::Time & /*time*/,
+                                                              const rclcpp::Duration & /*period*/) {
   auto lbr_wrench_command = rt_lbr_wrench_command_ptr_.readFromRT();
   if (!lbr_wrench_command || !(*lbr_wrench_command)) {
     return controller_interface::return_type::OK;
   }
-  for (std::size_t idx = 0; idx < lbr_fri_ros2::N_JNTS; ++idx) {
-    joint_position_command_interfaces_[idx].get().set_value(
-        (*lbr_wrench_command)->joint_position[idx]);
-  }
-  for (std::size_t idx = 0; idx < CARTESIAN_DOF; ++idx) {
-    wrench_command_interfaces_[idx].get().set_value((*lbr_wrench_command)->wrench[idx]);
-  }
+  // for (std::size_t idx = 0; idx < lbr_fri_ros2::N_JNTS; ++idx) {
+  //   joint_position_command_interfaces_[idx].get().set_value(
+  //       (*lbr_wrench_command)->joint_position[idx]);
+  // }
+  // for (std::size_t idx = 0; idx < CARTESIAN_DOF; ++idx) {
+  //   wrench_command_interfaces_[idx].get().set_value((*lbr_wrench_command)->wrench[idx]);
+  // }
+  return controller_interface::return_type::OK;
+}
+
+// expect just wrench command in this mode....
+controller_interface::return_type
+LBRWrenchCommandController::update_and_write_commands(const rclcpp::Time & /*time*/,
+                                                      const rclcpp::Duration & /*period*/) {
   return controller_interface::return_type::OK;
 }
 
@@ -68,6 +107,9 @@ LBRWrenchCommandController::on_configure(const rclcpp_lifecycle::State & /*previ
 
 controller_interface::CallbackReturn
 LBRWrenchCommandController::on_activate(const rclcpp_lifecycle::State & /*previous_state*/) {
+  if (!reference_state_interfaces_()) {
+    return controller_interface::CallbackReturn::ERROR;
+  }
   if (!reference_command_interfaces_()) {
     return controller_interface::CallbackReturn::ERROR;
   }
@@ -76,8 +118,37 @@ LBRWrenchCommandController::on_activate(const rclcpp_lifecycle::State & /*previo
 
 controller_interface::CallbackReturn
 LBRWrenchCommandController::on_deactivate(const rclcpp_lifecycle::State & /*previous_state*/) {
+  clear_state_interfaces_();
   clear_command_interfaces_();
   return controller_interface::CallbackReturn::SUCCESS;
+}
+
+bool LBRWrenchCommandController::reference_state_interfaces_() {
+  for (auto &state_interface : state_interfaces_) {
+    if (state_interface.get_interface_name() == hardware_interface::HW_IF_POSITION) {
+      joint_position_state_interfaces_.emplace_back(std::ref(state_interface));
+    }
+    if (state_interface.get_interface_name() == hardware_interface::HW_IF_VELOCITY) {
+      joint_velocity_state_interfaces_.emplace_back(std::ref(state_interface));
+    }
+  }
+  if (joint_position_state_interfaces_.size() != lbr_fri_ros2::N_JNTS) {
+    RCLCPP_ERROR(
+        this->get_node()->get_logger(),
+        "Number of joint position state interfaces '%ld' does not match the number of joints "
+        "in the robot '%d'.",
+        joint_position_state_interfaces_.size(), lbr_fri_ros2::N_JNTS);
+    return false;
+  }
+  if (joint_velocity_state_interfaces_.size() != lbr_fri_ros2::N_JNTS) {
+    RCLCPP_ERROR(
+        this->get_node()->get_logger(),
+        "Number of joint velocity state interfaces '%ld' does not match the number of joints "
+        "in the robot '%d'.",
+        joint_velocity_state_interfaces_.size(), lbr_fri_ros2::N_JNTS);
+    return false;
+  }
+  return true;
 }
 
 bool LBRWrenchCommandController::reference_command_interfaces_() {
@@ -106,6 +177,11 @@ bool LBRWrenchCommandController::reference_command_interfaces_() {
   return true;
 }
 
+void LBRWrenchCommandController::clear_state_interfaces_() {
+  joint_position_state_interfaces_.clear();
+  joint_velocity_state_interfaces_.clear();
+}
+
 void LBRWrenchCommandController::clear_command_interfaces_() {
   joint_position_command_interfaces_.clear();
   wrench_command_interfaces_.clear();
@@ -124,9 +200,38 @@ void LBRWrenchCommandController::configure_joint_names_() {
     joint_names_[i] = robot_name + "_A" + std::to_string(i + 1);
   }
 }
+
+void LBRWrenchCommandController::init_lbr_wrench_command_subscription_() {
+  lbr_wrench_command_subscription_ptr_ =
+      this->get_node()->create_subscription<lbr_fri_idl::msg::LBRWrenchCommand>(
+          "command/lbr_wrench_command", 1,
+          [this](const lbr_fri_idl::msg::LBRWrenchCommand::SharedPtr msg) {
+            rt_lbr_wrench_command_ptr_.writeFromNonRT(msg);
+          });
+}
+
+void LBRWrenchCommandController::init_wrench_command_subscription_() {
+  wrench_command_subscription_ptr_ =
+      this->get_node()->create_subscription<geometry_msgs::msg::Wrench>(
+          "command/wrench", 1, [this](const geometry_msgs::msg::Wrench::SharedPtr msg) {
+            rt_wrench_command_ptr_.writeFromNonRT(msg);
+          });
+}
+
+void LBRWrenchCommandController::reset_lbr_wrench_command_subscription_() {
+  if (wrench_command_subscription_ptr_) {
+    wrench_command_subscription_ptr_.reset();
+  }
+}
+void LBRWrenchCommandController::reset_wrench_command_subscription_() {
+
+  if (lbr_wrench_command_subscription_ptr_) {
+    lbr_wrench_command_subscription_ptr_.reset();
+  }
+}
 } // namespace lbr_ros2_control
 
 #include "pluginlib/class_list_macros.hpp"
 
 PLUGINLIB_EXPORT_CLASS(lbr_ros2_control::LBRWrenchCommandController,
-                       controller_interface::ControllerInterface)
+                       controller_interface::ChainableControllerInterface)
