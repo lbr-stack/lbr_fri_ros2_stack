2d3e12344dd62076c0b7a0d0089242adb3ba2911
mhubii <m.huber_1994@hotmail.de>
2025-11-28 17:14:07 +0000

    stop execution if any joints beyond limit (#314)
    (cherry picked from commit 038703ba26eda9aec23cbfb7a78067df94e63e8b)

--- lbr_ros2_control/src/controllers/lbr_state_broadcaster.cpp
+++ lbr_ros2_control/src/controllers/lbr_state_broadcaster.cpp
@@ -171,7 +171,7 @@ void LBRStateBroadcaster::configure_joint_names_() {
     throw std::runtime_error("Failed to configure joint names.");
   }
   std::string robot_name = this->get_node()->get_parameter("robot_name").as_string();
-  for (int i = 0; i < lbr_fri_ros2::N_JNTS; ++i) {
+  for (std::size_t i = 0; i < lbr_fri_ros2::N_JNTS; ++i) {
     joint_names_[i] = robot_name + "_A" + std::to_string(i + 1);
   }
 }
