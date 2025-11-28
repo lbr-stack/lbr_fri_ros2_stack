038703ba26eda9aec23cbfb7a78067df94e63e8b
mhubii <m.huber_1994@hotmail.de>
2025-11-28 17:14:07 +0000

    stop execution if any joints beyond limit (#314)

--- lbr_fri_ros2/src/math.cpp
+++ lbr_fri_ros2/src/math.cpp
@@ -8,4 +8,13 @@ bool norm_in_bounds(const double &x0, const double &x1, const double &x2, const 
 bool norm_in_bounds(const std::array<double, 3> &vec, const double &max) {
   return norm_in_bounds(vec[0], vec[1], vec[2], max);
 }
+bool all_jnts_in_bounds(const_jnt_array_t_ref vals, const_jnt_array_t_ref lower,
+                        const_jnt_array_t_ref upper) {
+  for (std::size_t i = 0; i < vals.size(); ++i) {
+    if (vals[i] < lower[i] || upper[i] < vals[i]) {
+      return false;
+    }
+  }
+  return true;
+}
 } // namespace lbr_fri_ros2
