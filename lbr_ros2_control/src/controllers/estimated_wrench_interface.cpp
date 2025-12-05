c9387836180eae101c475610c87a4f4a251a3a52
mhubii <m.huber_1994@hotmail.de>
2025-12-05 13:16:48 +0000

    finished chained wrench interface (#292)

--- lbr_ros2_control/src/controllers/estimated_wrench_interface.cpp
+++ lbr_ros2_control/src/controllers/estimated_wrench_interface.cpp
@@ -1,16 +1,16 @@
-#include "lbr_ros2_control/controllers/estimated_wrench_broadcaster.hpp"
+#include "lbr_ros2_control/controllers/estimated_wrench_interface.hpp"
 
 namespace lbr_ros2_control {
-EstimatedWrenchBroadcaster::EstimatedWrenchBroadcaster() {}
+EstimatedWrenchInterface::EstimatedWrenchInterface() {}
 
 controller_interface::InterfaceConfiguration
-EstimatedWrenchBroadcaster::command_interface_configuration() const {
+EstimatedWrenchInterface::command_interface_configuration() const {
   return controller_interface::InterfaceConfiguration{
       controller_interface::interface_configuration_type::NONE};
 }
 
 controller_interface::InterfaceConfiguration
-EstimatedWrenchBroadcaster::state_interface_configuration() const {
+EstimatedWrenchInterface::state_interface_configuration() const {
   controller_interface::InterfaceConfiguration interface_configuration;
   interface_configuration.type = controller_interface::interface_configuration_type::INDIVIDUAL;
 
@@ -22,18 +22,18 @@ EstimatedWrenchBroadcaster::state_interface_configuration() const {
   return interface_configuration;
 }
 
-controller_interface::CallbackReturn EstimatedWrenchBroadcaster::on_init() {
+controller_interface::CallbackReturn EstimatedWrenchInterface::on_init() {
   try {
     get_node()->declare_parameter("robot_name", "lbr");
-    get_node()->declare_parameter("wrench_estimator_parameters.chain_root", "lbr_link_0");
-    get_node()->declare_parameter("wrench_estimator_parameters.chain_tip", "lbr_link_ee");
-    get_node()->declare_parameter("wrench_estimator_parameters.damping", 0.2);
-    get_node()->declare_parameter("wrench_estimator_parameters.force_x_th", 2.0);
-    get_node()->declare_parameter("wrench_estimator_parameters.force_y_th", 2.0);
-    get_node()->declare_parameter("wrench_estimator_parameters.force_z_th", 2.0);
-    get_node()->declare_parameter("wrench_estimator_parameters.torque_x_th", 0.5);
-    get_node()->declare_parameter("wrench_estimator_parameters.torque_y_th", 0.5);
-    get_node()->declare_parameter("wrench_estimator_parameters.torque_z_th", 0.5);
+    get_node()->declare_parameter("wrench_estimator.chain_root", "lbr_link_0");
+    get_node()->declare_parameter("wrench_estimator.chain_tip", "lbr_link_ee");
+    get_node()->declare_parameter("wrench_estimator.damping", 0.2);
+    get_node()->declare_parameter("wrench_estimator.force_x_th", 2.0);
+    get_node()->declare_parameter("wrench_estimator.force_y_th", 2.0);
+    get_node()->declare_parameter("wrench_estimator.force_z_th", 2.0);
+    get_node()->declare_parameter("wrench_estimator.torque_x_th", 0.5);
+    get_node()->declare_parameter("wrench_estimator.torque_y_th", 0.5);
+    get_node()->declare_parameter("wrench_estimator.torque_z_th", 0.5);
     configure_joint_names_();
     configure_parameters_();
     wrench_estimator_ptr_ = std::make_unique<lbr_fri_ros2::WrenchEstimator>(
@@ -41,15 +41,15 @@ controller_interface::CallbackReturn EstimatedWrenchBroadcaster::on_init() {
   } catch (const std::exception &e) {
     RCLCPP_ERROR_STREAM(get_node()->get_logger(),
                         lbr_fri_ros2::ColorScheme::ERROR
-                            << "Failed to initialize estimated wrench broadcaster with: "
-                            << e.what() << "." << lbr_fri_ros2::ColorScheme::ENDC);
+                            << "Failed to initialize estimated wrench interface with: " << e.what()
+                            << "." << lbr_fri_ros2::ColorScheme::ENDC);
     return controller_interface::CallbackReturn::ERROR;
   }
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
 std::vector<hardware_interface::StateInterface>
-EstimatedWrenchBroadcaster::on_export_state_interfaces() {
+EstimatedWrenchInterface::on_export_state_interfaces() {
   std::vector<hardware_interface::StateInterface> state_interfaces;
   state_interfaces.emplace_back(std::string(get_node()->get_name()), HW_IF_FORCE_X, &wrench_[0]);
   state_interfaces.emplace_back(std::string(get_node()->get_name()), HW_IF_FORCE_Y, &wrench_[1]);
@@ -61,66 +61,63 @@ EstimatedWrenchBroadcaster::on_export_state_interfaces() {
 }
 
 std::vector<hardware_interface::CommandInterface>
-EstimatedWrenchBroadcaster::on_export_reference_interfaces() {
+EstimatedWrenchInterface::on_export_reference_interfaces() {
   return {};
 }
 
-bool EstimatedWrenchBroadcaster::on_set_chained_mode(bool chained_mode) {
-  RCLCPP_INFO(get_node()->get_logger(), "EstimatedWrenchBroadcaster::on_set_chained_mode");
-  if (chained_mode) {
-    // delete publisher
-  } else {
-    // create publisher
-  }
+bool EstimatedWrenchInterface::on_set_chained_mode(bool /*chained_mode*/) {
+  // broadcasters don't support chaining:
+  // https://github.com/ros-controls/ros2_controllers/issues/2052#issuecomment-3616731834
   return true;
-} // in chained mode expose force-torque state interface, else publish...
+}
 
 controller_interface::return_type
-EstimatedWrenchBroadcaster::update_reference_from_subscribers(const rclcpp::Time & /*time*/,
-                                                              const rclcpp::Duration & /*period*/) {
-  //   RCLCPP_INFO(get_node()->get_logger(),
-  //               "EstimatedWrenchBroadcaster::update_reference_from_subscribers");
+EstimatedWrenchInterface::update_reference_from_subscribers(const rclcpp::Time & /*time*/,
+                                                            const rclcpp::Duration & /*period*/) {
   return controller_interface::return_type::OK;
-} // do nothing...
+}
 
 controller_interface::return_type
-EstimatedWrenchBroadcaster::update_and_write_commands(const rclcpp::Time & /*time*/,
-                                                      const rclcpp::Duration & /*period*/) {
-  // get joint positions
+EstimatedWrenchInterface::update_and_write_commands(const rclcpp::Time & /*time*/,
+                                                    const rclcpp::Duration & /*period*/) {
+  // get joint positions and external torques
   if (!read_state_interfaces_()) {
     return controller_interface::return_type::OK;
   }
 
-  //   RCLCPP_INFO(get_node()->get_logger(),
-  //   "EstimatedWrenchBroadcaster::update_and_write_commands");
-  wrench_estimator_ptr_->set_q(joint_positions_);
-  wrench_estimator_ptr_->set_tau_ext(external_torques_);
-  wrench_estimator_ptr_->compute();
-  wrench_estimator_ptr_->get_f_ext_tf(wrench_);
+  // validate read states
+  if (!valid_states_()) {
+    return controller_interface::return_type::OK;
+  }
+
+  // estimate wrench given states
+  estimate_wrench_();
   return controller_interface::return_type::OK;
-} // do nothing...
+}
 
 controller_interface::CallbackReturn
-EstimatedWrenchBroadcaster::on_configure(const rclcpp_lifecycle::State & /*previous_state*/) {
+EstimatedWrenchInterface::on_configure(const rclcpp_lifecycle::State & /*previous_state*/) {
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
 controller_interface::CallbackReturn
-EstimatedWrenchBroadcaster::on_activate(const rclcpp_lifecycle::State & /*previous_state*/) {
+EstimatedWrenchInterface::on_activate(const rclcpp_lifecycle::State & /*previous_state*/) {
   if (!assign_state_interfaces_()) {
     release_state_interfaces_();
     return controller_interface::CallbackReturn::ERROR;
   }
+  nan_wrench_();
+  nan_referenced_states_();
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
 controller_interface::CallbackReturn
-EstimatedWrenchBroadcaster::on_deactivate(const rclcpp_lifecycle::State & /*previous_state*/) {
+EstimatedWrenchInterface::on_deactivate(const rclcpp_lifecycle::State & /*previous_state*/) {
   release_state_interfaces_();
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
-bool EstimatedWrenchBroadcaster::assign_state_interfaces_() {
+bool EstimatedWrenchInterface::assign_state_interfaces_() {
   for (auto &state_interface : state_interfaces_) {
     if (state_interface.get_interface_name() == hardware_interface::HW_IF_POSITION) {
       joint_position_state_interfaces_.push_back(std::ref(state_interface));
@@ -152,12 +149,12 @@ bool EstimatedWrenchBroadcaster::assign_state_interfaces_() {
   return true;
 }
 
-void EstimatedWrenchBroadcaster::release_state_interfaces_() {
+void EstimatedWrenchInterface::release_state_interfaces_() {
   joint_position_state_interfaces_.clear();
   external_torque_state_interfaces_.clear();
 }
 
-void EstimatedWrenchBroadcaster::configure_joint_names_() {
+void EstimatedWrenchInterface::configure_joint_names_() {
   if (joint_names_.size() != lbr_fri_ros2::N_JNTS) {
     RCLCPP_ERROR_STREAM(get_node()->get_logger(),
                         lbr_fri_ros2::ColorScheme::ERROR
@@ -172,25 +169,25 @@ void EstimatedWrenchBroadcaster::configure_joint_names_() {
   }
 }
 
-void EstimatedWrenchBroadcaster::configure_parameters_() {
+void EstimatedWrenchInterface::configure_parameters_() {
   wrench_estimator_parameters_.chain_root =
-      get_node()->get_parameter("wrench_estimator_parameters.chain_root").as_string();
+      get_node()->get_parameter("wrench_estimator.chain_root").as_string();
   wrench_estimator_parameters_.chain_tip =
-      get_node()->get_parameter("wrench_estimator_parameters.chain_tip").as_string();
+      get_node()->get_parameter("wrench_estimator.chain_tip").as_string();
   wrench_estimator_parameters_.damping =
-      get_node()->get_parameter("wrench_estimator_parameters.damping").as_double();
+      get_node()->get_parameter("wrench_estimator.damping").as_double();
   wrench_estimator_parameters_.force_x_th =
-      get_node()->get_parameter("wrench_estimator_parameters.force_x_th").as_double();
+      get_node()->get_parameter("wrench_estimator.force_x_th").as_double();
   wrench_estimator_parameters_.force_y_th =
-      get_node()->get_parameter("wrench_estimator_parameters.force_y_th").as_double();
+      get_node()->get_parameter("wrench_estimator.force_y_th").as_double();
   wrench_estimator_parameters_.force_z_th =
-      get_node()->get_parameter("wrench_estimator_parameters.force_z_th").as_double();
+      get_node()->get_parameter("wrench_estimator.force_z_th").as_double();
   wrench_estimator_parameters_.torque_x_th =
-      get_node()->get_parameter("wrench_estimator_parameters.torque_x_th").as_double();
+      get_node()->get_parameter("wrench_estimator.torque_x_th").as_double();
   wrench_estimator_parameters_.torque_y_th =
-      get_node()->get_parameter("wrench_estimator_parameters.torque_y_th").as_double();
+      get_node()->get_parameter("wrench_estimator.torque_y_th").as_double();
   wrench_estimator_parameters_.torque_z_th =
-      get_node()->get_parameter("wrench_estimator_parameters.torque_z_th").as_double();
+      get_node()->get_parameter("wrench_estimator.torque_z_th").as_double();
   if (!wrench_estimator_parameters_.valid()) {
     RCLCPP_ERROR_STREAM(get_node()->get_logger(), lbr_fri_ros2::ColorScheme::ERROR
                                                       << "Invalid wrench estimator parameters."
@@ -199,33 +196,58 @@ void EstimatedWrenchBroadcaster::configure_parameters_() {
   }
 }
 
-bool EstimatedWrenchBroadcaster::read_state_interfaces_() {
+bool EstimatedWrenchInterface::read_state_interfaces_() {
   for (std::size_t i = 0; i < lbr_fri_ros2::N_JNTS; ++i) {
-    auto q_i = joint_position_state_interfaces_[i].get().get_optional();
-    if (!q_i.has_value()) {
+    auto joint_position = joint_position_state_interfaces_[i].get().get_optional();
+    if (!joint_position.has_value()) {
       RCLCPP_WARN_STREAM(get_node()->get_logger(), lbr_fri_ros2::ColorScheme::WARNING
                                                        << "Failed to get joint position for joint '"
                                                        << joint_names_[i] << "'."
                                                        << lbr_fri_ros2::ColorScheme::ENDC);
       return false;
     }
-    joint_positions_[i] = *q_i;
+    joint_positions_[i] = *joint_position;
 
-    auto tau_ext_i = external_torque_state_interfaces_[i].get().get_optional();
-    if (!tau_ext_i.has_value()) {
+    auto external_torque = external_torque_state_interfaces_[i].get().get_optional();
+    if (!external_torque.has_value()) {
       RCLCPP_WARN_STREAM(get_node()->get_logger(),
                          lbr_fri_ros2::ColorScheme::WARNING
                              << "Failed to get external torque for joint '" << joint_names_[i]
                              << "'." << lbr_fri_ros2::ColorScheme::ENDC);
       return false;
     }
-    external_torques_[i] = *tau_ext_i;
+    external_torques_[i] = *external_torque;
   }
   return true;
+}
+
+bool EstimatedWrenchInterface::valid_states_() {
+  for (size_t i = 0; i < lbr_fri_ros2::N_JNTS; ++i) {
+    if (std::isnan(joint_positions_[i]) || std::isnan(external_torques_[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void EstimatedWrenchInterface::nan_wrench_() {
+  wrench_.fill(std::numeric_limits<double>::quiet_NaN());
+}
+
+void EstimatedWrenchInterface::nan_referenced_states_() {
+  joint_positions_.fill(std::numeric_limits<double>::quiet_NaN());
+  external_torques_.fill(std::numeric_limits<double>::quiet_NaN());
+}
+
+void EstimatedWrenchInterface::estimate_wrench_() {
+  wrench_estimator_ptr_->set_q(joint_positions_);
+  wrench_estimator_ptr_->set_tau_ext(external_torques_);
+  wrench_estimator_ptr_->compute();
+  wrench_estimator_ptr_->get_f_ext_tf(wrench_);
 }
 } // namespace lbr_ros2_control
 
 #include "pluginlib/class_list_macros.hpp"
 
-PLUGINLIB_EXPORT_CLASS(lbr_ros2_control::EstimatedWrenchBroadcaster,
+PLUGINLIB_EXPORT_CLASS(lbr_ros2_control::EstimatedWrenchInterface,
                        controller_interface::ChainableControllerInterface)
