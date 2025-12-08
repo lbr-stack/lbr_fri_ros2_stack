f49490d0ec6697fe9b811d632fd1f6d9291229b1
mhubii <m.huber_1994@hotmail.de>
2025-12-08 10:56:13 +0000

    load controllers from single spawner (#329)

--- lbr_bringup/launch/hardware.launch.py
+++ lbr_bringup/launch/hardware.launch.py
@@ -1,6 +1,4 @@
 from launch import LaunchDescription
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnExecutionComplete, OnProcessStart
 from launch.substitutions import LaunchConfiguration
 from lbr_bringup.description import LBRDescriptionMixin
 from lbr_bringup.ros2_control import LBRROS2ControlMixin
@@ -31,45 +29,16 @@ def generate_launch_description() -> LaunchDescription:
     ros2_control_node = LBRROS2ControlMixin.node_ros2_control(use_sim_time=False)
     ld.add_action(ros2_control_node)
 
-    # controllers on ros2 control node start
-    joint_state_broadcaster = LBRROS2ControlMixin.node_controller_spawner(
-        controller="joint_state_broadcaster"
-    )
-    estimated_wrench_interface = LBRROS2ControlMixin.node_controller_spawner(
-        controller="estimated_wrench_interface"
-    )
-    lbr_state_broadcaster = LBRROS2ControlMixin.node_controller_spawner(
-        controller="lbr_state_broadcaster"
-    )
-
-    preceding_controllers_event_handler = RegisterEventHandler(
-        OnProcessStart(
-            target_action=ros2_control_node,
-            on_start=[
-                joint_state_broadcaster,
-                estimated_wrench_interface,
-                lbr_state_broadcaster,
-            ],
+    # spawn controllers
+    ld.add_action(
+        LBRROS2ControlMixin.node_controller_spawner(
+            controllers=[
+                "joint_state_broadcaster",
+                "estimated_wrench_interface",
+                "lbr_state_broadcaster",
+                "force_torque_broadcaster",
+                LaunchConfiguration("ctrl"),
+            ]
         )
     )
-    ld.add_action(preceding_controllers_event_handler)
-
-    # controllers on estimated wrench interface
-    force_torque_broadcaster = LBRROS2ControlMixin.node_controller_spawner(
-        controller="force_torque_broadcaster"
-    )
-    controller = LBRROS2ControlMixin.node_controller_spawner(
-        controller=LaunchConfiguration("ctrl")
-    )
-
-    controller_event_handler = RegisterEventHandler(
-        OnExecutionComplete(
-            target_action=estimated_wrench_interface,  # estimated wrench interface is chained and exposes external wrench estimate state interfaces
-            on_completion=[
-                force_torque_broadcaster,
-                controller,
-            ],
-        )
-    )
-    ld.add_action(controller_event_handler)
     return ld
