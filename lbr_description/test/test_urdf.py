9269e7811519954c39da00ae46a0e7812471d75c
mhubii <m.huber_1994@hotmail.de>
2026-01-21 14:07:14 +0000

    fix tests (#366)

--- lbr_description/test/test_urdf.py
+++ lbr_description/test/test_urdf.py
@@ -35,7 +35,7 @@ def setup_xml_and_reference(kuka_id: str) -> Tuple[str, LBRSpecification]:
         f"{lbr_specification.name}.xacro",
     )
 
-    xml = xacro.process(path)
+    xml = xacro.process(path, mappings={"robot_name": lbr_specification.name})
     return xml, lbr_specification
 
 
@@ -71,11 +71,15 @@ def test_position_limits(
         if joint.type == "revolute":
             urdf_min_position = joint.limit.lower
             kuka_min_position = math.radians(
-                lbr_specification.joint_limits[joint.name].min_position
+                lbr_specification.joint_limits[
+                    joint.name.replace(f"{lbr_specification.name}_", "")
+                ].min_position
             )
             if not math.isclose(
                 urdf_min_position
-                + position_limit_restriction,  # note that we enforce 1 degree stricter limits to avoid hardware limits
+                - position_limit_restriction
+                / 180
+                * math.pi,  # note that we enforce 1 degree stricter limits to avoid hardware limits
                 kuka_min_position,
                 abs_tol=abs_tol,
             ):
@@ -85,11 +89,15 @@ def test_position_limits(
 
             urdf_max_position = joint.limit.upper
             kuka_max_position = math.radians(
-                lbr_specification.joint_limits[joint.name].max_position
+                lbr_specification.joint_limits[
+                    joint.name.replace(f"{lbr_specification.name}_", "")
+                ].max_position
             )
             if not math.isclose(
                 urdf_max_position
-                - position_limit_restriction,  # note that we enforce 1 degree stricter limits to avoid hardware limits
+                + position_limit_restriction
+                / 180
+                * math.pi,  # note that we enforce 1 degree stricter limits to avoid hardware limits
                 kuka_max_position,
                 abs_tol=abs_tol,
             ):
@@ -109,7 +117,9 @@ def test_velocity_limits(
         if joint.type == "revolute":
             urdf_max_velocity = joint.limit.velocity
             kuka_max_velcoity = math.radians(
-                lbr_specification.joint_limits[joint.name].max_velocity
+                lbr_specification.joint_limits[
+                    joint.name.replace(f"{lbr_specification.name}_", "")
+                ].max_velocity
             )
             if not math.isclose(urdf_max_velocity, kuka_max_velcoity, abs_tol=abs_tol):
                 raise ValueError(
