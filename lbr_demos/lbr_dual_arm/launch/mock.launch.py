c37422a4d2e4a935b21190f2f16f7e0345b46497
Kenichi Maeda <kenichi.maeda121@gmail.com>
2026-03-31 11:10:28 -0400

    Format dual-arm demo launch files
    (cherry picked from commit 4baf5c4b35988e69070d43c6cb93e3f1ab62a0e4)

--- lbr_demos/lbr_dual_arm/launch/mock.launch.py
+++ lbr_demos/lbr_dual_arm/launch/mock.launch.py
@@ -1,7 +1,12 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, RegisterEventHandler
 from launch.event_handlers import OnProcessStart
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from lbr_bringup.ros2_control import LBRROS2ControlMixin
