41da2475a7527e48d4432c1fa27e56320fe90eca
Miguel Prada <miguel.prada@tecnalia.com>
2026-05-26 11:48:09 +0200

    Adjust auxiliary interface name in admittance controller
    (cherry picked from commit 735e91a9d75acd43b94f6cd1774ea016e66c7e45)
    (cherry picked from commit b59b82f6cb49fa17594e87835e58085114aabd5c)

--- lbr_ros2_control/src/controllers/admittance_controller.cpp
+++ lbr_ros2_control/src/controllers/admittance_controller.cpp
@@ -29,7 +29,8 @@ AdmittanceController::state_interface_configuration() const {
   }
 
   // additional state interfaces
-  interface_configuration.names.push_back(std::string(HW_IF_AUXILIARY_PREFIX) + "/" +
+  interface_configuration.names.push_back(this->get_node()->get_parameter("robot_name").as_string() + "_" +
+                                          std::string(HW_IF_AUXILIARY_PREFIX) + "/" +
                                           HW_IF_SESSION_STATE);
   return interface_configuration;
 }
